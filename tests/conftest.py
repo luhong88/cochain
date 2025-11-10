f60bd9b735f41677d0e03c66bce0f662e0d6ea30
Lu Hong <honglu88@gmail.com>
2025-11-09 22:24:45 -0800

    more tests for cotan laplacian

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,27 @@
+import numpy as np
 import pytest
+import torch as t
+import trimesh
 
 import cochain.datasets as datasets
+from cochain.complex import Simplicial2Complex
 
 
 @pytest.fixture
 def two_tris_mesh():
     return datasets.load_two_tris_mesh()
+
+
+@pytest.fixture
+def icosphere_mesh():
+    trimesh_sphere = trimesh.creation.icosphere(subdivisions=1)
+
+    vert_coords_np = np.asarray(trimesh_sphere.vertices)
+    tris_np = np.asarray(trimesh_sphere.faces)
+
+    vert_coords_t = t.from_numpy(vert_coords_np).to(dtype=t.float)
+    tris_t = t.from_numpy(tris_np)
+
+    cochain_sphere = Simplicial2Complex.from_mesh(vert_coords_t, tris_t)
+
+    return cochain_sphere
--- tests/test_cotan_laplacian.py
+++ tests/test_cotan_laplacian.py
@@ -1,6 +1,36 @@
 import torch as t
 
-from cochain.geometry import _DifferentiableCotanLaplacian
+from cochain.geometry import _DifferentiableCotanLaplacian, cotan_laplacian
+
+
+def test_cotan_laplacian_kernel(icosphere_mesh):
+    """
+    The Laplacian acting on a constant function over the vertices should return
+    the zero vector. This can be checked by comparing the row sum of the matrix
+    with the zero vector.
+    """
+    sphere_L0 = cotan_laplacian(icosphere_mesh)
+    row_sum = sphere_L0.to_dense().sum(dim=-1)
+    assert t.allclose(row_sum, t.tensor(0.0), atol=1e-6)
+
+
+def test_cotan_laplacian_symmetry(icosphere_mesh):
+    """
+    The Laplacian should be a symmetric matrix.
+    """
+    sphere_L0 = cotan_laplacian(icosphere_mesh)
+    sphere_L0_dense = sphere_L0.to_dense()
+    assert t.allclose(sphere_L0_dense, sphere_L0_dense.T, atol=1e-6)
+
+
+def test_cotan_laplacian_PSD(icosphere_mesh):
+    """
+    The Laplacian should be a positive semi-definite matrix.
+    """
+    sphere_L0 = cotan_laplacian(icosphere_mesh)
+    sphere_L0_dense = sphere_L0.to_dense()
+    eigs = t.linalg.eigvalsh(sphere_L0_dense)
+    assert eigs.min() >= -1e-6
 
 
 def test_cotan_laplacian_gradcheck(two_tris_mesh):
