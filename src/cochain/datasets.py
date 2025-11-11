a00ea7c3c7b785ae362b5da07361f752234f030d
Lu Hong <honglu88@gmail.com>
2025-11-10 20:39:49 -0800

    check autograd

--- src/cochain/datasets.py
+++ src/cochain/datasets.py
@@ -7,11 +7,9 @@ def load_two_tris_mesh() -> Simplicial2Complex:
     """
     A simple 2D mesh embedded in R^3 composed of two triangles sharing one edge.
     """
-    # TODO: the use of t.double is required for gradcheck(), but could be inconvenient
-    # in other use cases where t.float is expected.
     return Simplicial2Complex.from_mesh(
         vert_coords=t.Tensor(
             [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-        ).to(dtype=t.double),
+        ),
         tris=t.LongTensor([[0, 1, 2], [1, 2, 3]]),
     )
--- tests/test_cotan_laplacian.py
+++ tests/test_cotan_laplacian.py
@@ -1,6 +1,10 @@
 import torch as t
 
-from cochain.geometry import _DifferentiableCotanLaplacian, cotan_laplacian
+from cochain.geometry import (
+    _cotan_laplacian,
+    _DifferentiableCotanLaplacian,
+    cotan_laplacian,
+)
 
 
 def test_cotan_laplacian_kernel(icosphere_mesh):
@@ -33,16 +37,38 @@ def test_cotan_laplacian_PSD(icosphere_mesh):
     assert eigs.min() >= -1e-6
 
 
-def test_cotan_laplacian_gradcheck(icosphere_mesh):
+def test_cotan_laplacian_autograd(icosphere_mesh):
+    """
+    Check that the custom gradient matches the automatic gradient.
+    """
+    icosphere_mesh.vert_coords.requires_grad = True
+    sphere_L0_custom = cotan_laplacian(icosphere_mesh).to_dense()
+    y_custom = (sphere_L0_custom**2).sum()
+    custom_grad = t.autograd.grad(y_custom, icosphere_mesh.vert_coords)
+
+    sphere_L0_auto = _cotan_laplacian(
+        icosphere_mesh.vert_coords, icosphere_mesh.tris
+    ).to_dense()
+    y_auto = (sphere_L0_auto**2).sum()
+    auto_grad = t.autograd.grad(y_auto, icosphere_mesh.vert_coords)
+
+    assert t.allclose(custom_grad[0], auto_grad[0], atol=1e-4)
+
+
+def test_cotan_laplacian_gradcheck(two_tris_mesh):
+    """
+    Check that the custom gradient for the Laplacian agrees with numerical gradient.
+    """
+
     def gradcheck_func(vert_coords, tris):
         L_sparse = _DifferentiableCotanLaplacian.apply(vert_coords, tris)
         L_dense = L_sparse.to_dense()
 
         return (L_dense**2).sum()
 
-    vert_coords_double = icosphere_mesh.vert_coords.to(t.double)
+    vert_coords_double = two_tris_mesh.vert_coords.to(t.double)
     vert_coords_double.requires_grad = True
 
     assert t.autograd.gradcheck(
-        gradcheck_func, (vert_coords_double, icosphere_mesh.tris)
+        gradcheck_func, (vert_coords_double, two_tris_mesh.tris)
     )
