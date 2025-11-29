f6498c49d972bb9065491c1b585b75d31be3bf95
Lu Hong <honglu88@gmail.com>
2025-11-28 20:29:11 -0800

    docstrings and comments

--- src/cochain/geometry/tet/tet_masses.py
+++ src/cochain/geometry/tet/tet_masses.py
@@ -32,6 +32,74 @@ def mass_0(tet_mesh: SimplicialComplex) -> Float[t.Tensor, "vert"]:
     return diag
 
 
+def _bary_coord_grad_inner_prods(
+    tet_signed_vols: Float[t.Tensor, "tet"],
+    d_signed_vols_d_vert_coords: Float[t.Tensor, "tet 4 3"],
+) -> tuple[Float[t.Tensor, "tet 1"], Float[t.Tensor, "tet 4 4"]]:
+    """
+    For a tet, let lambda_x(p) be the barycentric coordinate function for p wrt
+    a vertex x of the tet. This function computes all pairwise inner products
+    of the barycentric coordinate gradients wrt each pair of vertices; i.e., it
+    computes <grad_p[lambda_x(p)], grad_p[lambda_y(p)]> for all vertices x and y.
+    """
+    # The gradient of lambda_i(p) wrt p is given by grad_i(vol_ijkl)/vol_ijkl, a
+    # constant wrt p.
+    bary_coords_grad: Float[t.Tensor, "tet 4 3"] = (
+        d_signed_vols_d_vert_coords / tet_signed_vols
+    )
+
+    bary_coords_grad_dot: Float[t.Tensor, "tet 4 4"] = t.einsum(
+        "tic,tjc->tij", bary_coords_grad, bary_coords_grad
+    )
+
+    return bary_coords_grad_dot
+
+
+def _tet_edge_faces(
+    tet_mesh: SimplicialComplex,
+) -> tuple[Float[t.Tensor, "tet 6"], Integer[t.LongTensor, "tet 6"]]:
+    """
+    Enumerate all edges for each tet and find their orientations and indices on
+    the tet_mesh.edges list.
+    """
+    device = tet_mesh.vert_coords.device
+
+    n_verts = tet_mesh.n_verts
+
+    # Enumerate all unique edges via their vertex position in the etet.
+    i, j, k, l = 0, 1, 2, 3
+    unique_edges = t.tensor(
+        [[i, j], [i, k], [j, k], [j, l], [k, l], [i, l]], dtype=t.long, device=device
+    )
+
+    # For each tet and each unique edge pair, find the orientations of the edges
+    # and their indices on the list of unique, canonical edges (tet_mesh.edges).
+    whitney_edges: Float[t.Tensor, "tet*6 2"] = tet_mesh.tets[:, unique_edges].flatten(
+        end_dim=-2
+    )
+
+    # Same method as used in the construction of coboundary operators to use
+    # sort() to identify edge orientations.
+    whitney_canon_edges, whitney_edge_orientations = whitney_edges.sort(dim=-1)
+    whitney_edge_signs: Float[t.Tensor, "tet 6"] = t.where(
+        whitney_edge_orientations[:, 1] > 0, whitney_edge_orientations[:, 1], -1
+    ).view(-1, 6)
+
+    # This assumes that the edge indices in tet_mesh.edges are already in canonical
+    # orders.
+    unique_canon_edges_packed = tet_mesh.edges[:, 0] * n_verts + tet_mesh.edges[:, 1]
+    canon_edges_packed_sorted, canon_edges_idx = t.sort(unique_canon_edges_packed)
+
+    whitney_edges_packed = (
+        whitney_canon_edges[:, 0] * n_verts + whitney_canon_edges[:, 1]
+    )
+    whitney_edges_idx: Float[t.Tensor, "tet 6"] = canon_edges_idx[
+        t.searchsorted(canon_edges_packed_sorted, whitney_edges_packed)
+    ].view(-1, 6)
+
+    return whitney_edge_signs, whitney_edges_idx
+
+
 def mass_1(tet_mesh: SimplicialComplex) -> Float[t.Tensor, "edge edge"]:
     """
     Compute the Galerkin edge/1-form mass matrix.
@@ -134,89 +202,11 @@ def mass_1(tet_mesh: SimplicialComplex) -> Float[t.Tensor, "edge edge"]:
     return mass
 
 
-def _bary_coord_grad_inner_prods(
-    tet_signed_vols: Float[t.Tensor, "tet"],
-    d_signed_vols_d_vert_coords: Float[t.Tensor, "tet 4 3"],
-) -> tuple[Float[t.Tensor, "tet 1"], Float[t.Tensor, "tet 4 4"]]:
-    # For a tet ijkl, let p be a position vector inside ijkl and let lambda_i(p)
-    # be the barycentric coordinate of p wrt vertex i. The gradient of lambda_i(p)
-    # wrt p is given by grad_i(vol_ijkl)/vol_ijkl, a constant wrt p.
-    bary_coords_grad: Float[t.Tensor, "tet 4 3"] = (
-        d_signed_vols_d_vert_coords / tet_signed_vols
-    )
-    # For each tet ijkl, compute all pairwise inner products of the barycentric
-    # coordinate gradients wrt each pair of vertices.
-    bary_coords_grad_dot: Float[t.Tensor, "tet 4 4"] = t.einsum(
-        "tic,tjc->tij", bary_coords_grad, bary_coords_grad
-    )
-
-    return bary_coords_grad_dot
-
-
-def _tet_edge_faces(
-    tet_mesh: SimplicialComplex,
-) -> tuple[Float[t.Tensor, "tet 6"], Integer[t.LongTensor, "tet 6"]]:
-    device = tet_mesh.vert_coords.device
-
-    n_verts = tet_mesh.n_verts
-
-    # Enumerate all unique edges via their vertex position in the etet.
-    i, j, k, l = 0, 1, 2, 3
-    unique_edges = t.tensor(
-        [[i, j], [i, k], [j, k], [j, l], [k, l], [i, l]], dtype=t.long, device=device
-    )
-
-    # For each tet and each unique edge pair, find the orientations of the edges
-    # and their indices on the list of unique, canonical edges (tet_mesh.edges).
-    whitney_edges: Float[t.Tensor, "tet*6 2"] = tet_mesh.tets[:, unique_edges].flatten(
-        end_dim=-2
-    )
-
-    # Same method as used in the construction of coboundary operators to use
-    # sort() to identify edge orientations.
-    whitney_canon_edges, whitney_edge_orientations = whitney_edges.sort(dim=-1)
-    whitney_edge_signs: Float[t.Tensor, "tet 6"] = t.where(
-        whitney_edge_orientations[:, 1] > 0, whitney_edge_orientations[:, 1], -1
-    ).view(-1, 6)
-
-    # This assumes that the edge indices in tet_mesh.edges are already in canonical
-    # orders.
-    unique_canon_edges_packed = tet_mesh.edges[:, 0] * n_verts + tet_mesh.edges[:, 1]
-    canon_edges_packed_sorted, canon_edges_idx = t.sort(unique_canon_edges_packed)
-
-    whitney_edges_packed = (
-        whitney_canon_edges[:, 0] * n_verts + whitney_canon_edges[:, 1]
-    )
-    whitney_edges_idx: Float[t.Tensor, "tet 6"] = canon_edges_idx[
-        t.searchsorted(canon_edges_packed_sorted, whitney_edges_packed)
-    ].view(-1, 6)
-
-    return whitney_edge_signs, whitney_edges_idx
-
-
 def d_mass_1_d_vert_coords(
     tet_mesh: SimplicialComplex,
 ) -> Float[t.Tensor, "edge edge vert 3"]:
     """
-    D_xy = <grad_x[V], grad_y[V]>/V**2
-
-    find its gradient wrt p:
-
-    (grad_p[<grad_x[V], grad_y[V]>]*V**2 - 2V*<grad_x[V], grad_y[V]>*grad_p[V])/V**4
-    =(hess_xp[V]*grad_y[V] + hess_yp[V]*grad_x[V])/V**2 - 2*D_xy*grad_p[V])/V
-
-
-    I_xy=V*(1 + delta_xy)/20
-
-    find its gradient wrt p:
-
-    grad_p[V]*(1 + delta_xy)/20
-
-    W_xy,rs = I_xr*D_ys - I_xs*D_yr - I_yr*D_xs + I_ys*D_xr
-
-    grad_p[W_xy,rs] = I_xr*D_ys - I_xs*D_yr - I_yr*D_xs + I_ys*D_xr
-
-
+    Compute the Jacobian of the 1-form mass matrix wrt the vertex coordinates.
     """
     vert_coords: Float[t.Tensor, "vert 3"] = tet_mesh.vert_coords
     tets: Integer[t.LongTensor, "tet 4"] = tet_mesh.tets
@@ -228,6 +218,10 @@ def d_mass_1_d_vert_coords(
     n_edges = tet_mesh.n_edges
     n_verts = tet_mesh.n_verts
 
+    # For D_xy, the inner products of the gradients of the barycentric coordinates,
+    # its Jacobian wrt vertex p is given by
+    #     grad_p[D_xy] = (hess_xp[V]*grad_y[V] + hess_yp[V]*grad_x[V])/V**2
+    #                    - 2*D_xy*grad_p[V])/V
     tet_signed_vols: Float[t.Tensor, "tet"] = _tet_signed_vols(vert_coords, tets)
     tet_signs = tet_signed_vols.sign()
     d_signed_vols_d_vert_coords: Float[t.Tensor, "tet 4 3"] = (
@@ -252,6 +246,8 @@ def d_mass_1_d_vert_coords(
         -1, 1, 1, 4, 3
     ) / tet_signed_vols.view(-1, 1, 1, 1, 1)
 
+    # For I_xy, the pairwise integrals of the barycentric coordinates, its gradient
+    # wrt vertex p is given by grad_p[I_xy] =  grad_p[V]*(1 + delta_xy)/20
     bary_coords_int: Float[t.Tensor, "tet x=4 y=4 1 1"] = t.abs(
         tet_signed_vols / 20.0
     ) * (
@@ -279,17 +275,38 @@ def d_mass_1_d_vert_coords(
     r_idx = unique_edges[:, 0][None, :]
     s_idx = unique_edges[:, 1][None, :]
 
-    whitney_inner_prods_grad: Float[t.Tensor, "tet xy=6 rs=6 p=4 3"] = (
-        bary_coords_int_grad[:, x_idx, r_idx] * bary_coords_grad_dot[:, y_idx, s_idx]
-        + bary_coords_int[:, x_idx, r_idx] * bary_coords_grad_dot_grad[:, y_idx, s_idx]
-        - bary_coords_int_grad[:, x_idx, s_idx] * bary_coords_grad_dot[:, y_idx, r_idx]
-        - bary_coords_int[:, x_idx, s_idx] * bary_coords_grad_dot_grad[:, y_idx, r_idx]
-        - bary_coords_int_grad[:, y_idx, r_idx] * bary_coords_grad_dot[:, x_idx, s_idx]
-        - bary_coords_int[:, y_idx, r_idx] * bary_coords_grad_dot_grad[:, x_idx, s_idx]
-        + bary_coords_int_grad[:, y_idx, s_idx] * bary_coords_grad_dot[:, x_idx, r_idx]
-        + bary_coords_int[:, y_idx, s_idx] * bary_coords_grad_dot_grad[:, x_idx, r_idx]
+    # Find the gradient of the mass matrix element W_xy,rs using the product rule
+    whitney_inner_prods_grad: Float[t.Tensor, "tet xy=6 rs=6 p=4 3"] = t.zeros(
+        (n_tets, 6, 6, 4, 3), dtype=dtype, device=device
     )
 
+    # Use inplace operations for better peak memory usage.
+    whitney_inner_prods_grad.add_(
+        bary_coords_int_grad[:, x_idx, r_idx] * bary_coords_grad_dot[:, y_idx, s_idx]
+    )
+    whitney_inner_prods_grad.add_(
+        bary_coords_int[:, x_idx, r_idx] * bary_coords_grad_dot_grad[:, y_idx, s_idx]
+    )
+    whitney_inner_prods_grad.subtract_(
+        bary_coords_int_grad[:, x_idx, s_idx] * bary_coords_grad_dot[:, y_idx, r_idx]
+    )
+    whitney_inner_prods_grad.subtract_(
+        bary_coords_int[:, x_idx, s_idx] * bary_coords_grad_dot_grad[:, y_idx, r_idx]
+    )
+    whitney_inner_prods_grad.subtract_(
+        bary_coords_int_grad[:, y_idx, r_idx] * bary_coords_grad_dot[:, x_idx, s_idx]
+    )
+    whitney_inner_prods_grad.subtract_(
+        bary_coords_int[:, y_idx, r_idx] * bary_coords_grad_dot_grad[:, x_idx, s_idx]
+    )
+    whitney_inner_prods_grad.add_(
+        bary_coords_int_grad[:, y_idx, s_idx] * bary_coords_grad_dot[:, x_idx, r_idx]
+    )
+    whitney_inner_prods_grad.add_(
+        bary_coords_int[:, y_idx, s_idx] * bary_coords_grad_dot_grad[:, x_idx, r_idx]
+    )
+
+    # Scatter the gradients to a sparse tensor.
     whitney_edge_signs, whitney_edges_idx = _tet_edge_faces(tet_mesh)
 
     whitney_inner_prods_grad_flat_signed: Float[t.Tensor, "tet 144"] = (
@@ -302,14 +319,13 @@ def d_mass_1_d_vert_coords(
     dMdV_idx_rs = whitney_edges_idx.view(-1, 1, 6, 1).expand(-1, 6, 6, 4).flatten()
     dMdV_idx_p = tet_mesh.tets.view(-1, 1, 1, 4).expand(-1, 6, 6, 4).flatten()
 
-    # Assemble the mass matrix.
-    mass = t.sparse_coo_tensor(
+    dMdV = t.sparse_coo_tensor(
         t.vstack((dMdV_idx_xy, dMdV_idx_rs, dMdV_idx_p)),
         whitney_inner_prods_grad_flat_signed.flatten(end_dim=-2),
         (n_edges, n_edges, n_verts, 3),
     ).coalesce()
 
-    return mass
+    return dMdV
 
 
 def _whitney_2_form_inner_prods(
