95c87ebf7f26aac7e4fb76e0d87a8c5274807aed
Lu Hong <honglu88@gmail.com>
2025-12-16 21:27:10 -0800

    gradient through values only

--- src/cochain/utils/sparse_solver.py
+++ src/cochain/utils/sparse_solver.py
@@ -22,51 +22,47 @@ if TYPE_CHECKING:
     import cupyx.scipy.sparse.linalg as cp_sp_linalg
 
 
-def _csc_torch_to_cupy(torch_sp_csc: t.Tensor) -> cp_sp.csc_matrix:
-    src = torch_sp_csc.detach()
-
-    shape = tuple(src.shape)
-
-    # Note that cupy will force downcasting from int64 to int32.
-    ccol_idx = cp.from_dlpack(src.ccol_indices())
-    row_idx = cp.from_dlpack(src.row_indices())
-
-    val = cp.from_dlpack(src.values())
-
-    cupy_sp_csc = cp_sp.csc_matrix((val, row_idx, ccol_idx), shape=shape)
-
-    return cupy_sp_csc
-
-
 class _CuPySuperLUWrapper(t.autograd.Function):
     @staticmethod
     def forward(
-        A: Float[t.Tensor, "r c"],
+        A_val: Float[t.Tensor, " nnz"],
         A_coo_idx: Integer[t.LongTensor, "2 nnz"],
+        A_shape: tuple[int, int],
         b: Float[t.Tensor, " r"],
         splu_kwargs: dict[str, Any],
     ) -> tuple[Float[t.Tensor, " c"], cp_sp_linalg.SuperLU]:
         # A must be 1) in sparse csc format and 2) moved to the cuda device
-        A_cp = _csc_torch_to_cupy(A)
+        A_cp: Float[cp_sp.csc_matrix, "r c"] = cp_sp.coo_matrix(
+            (
+                cp.from_dlpack(A_val.detach()),
+                (
+                    cp.from_dlpack(A_coo_idx.detach()[0]),
+                    cp.from_dlpack(A_coo_idx.detach()[1]),
+                ),
+            ),
+            shape=A_shape,
+        ).tocsc()
         solver = cp_sp_linalg.splu(A_cp, **splu_kwargs)
         x = t.from_dlpack(solver.solve(cp.from_dlpack(b.detach()), trans="N"))
         return x, solver
 
     @staticmethod
     def setup_context(ctx, inputs, output):
-        A, A_coo_idx, b, splu_kwargs = inputs
+        A_val, A_coo_idx, A_shape, b, splu_kwargs = inputs
 
         x, solver = output
         ctx.mark_non_differentiable(solver)
 
         ctx.save_for_backward(A_coo_idx, x)
         ctx.solver = solver
-        ctx.A_shape = tuple(A.shape)
+        ctx.A_shape = A_shape
 
     @staticmethod
     def backward(
         ctx, dLdx: Float[t.Tensor, " c"], _
-    ) -> tuple[Float[t.Tensor, "r c"] | None, None, Float[t.Tensor, " r"] | None, None]:
+    ) -> tuple[
+        Float[t.Tensor, " nnz"] | None, None, None, Float[t.Tensor, " r"] | None, None
+    ]:
         """
         Let x be the solution to the linear system A@x = b. To find the gradient
         of some loss L wrt A and b, the adjoint method first defines a Lagrangian
@@ -75,10 +71,11 @@ class _CuPySuperLUWrapper(t.autograd.Function):
         equal whenver A@x = b. From this condition, it follows that dLdb is given
         by lambda, and dLdA is given by the outer product -lambda@x.T.
         """
-        needs_grad_A, _, needs_grad_b, _ = ctx.needs_input_grad
+        needs_grad_A_val = ctx.needs_input_grad[0]
+        needs_grad_b = ctx.needs_input_grad[-2]
 
-        if not (needs_grad_A or needs_grad_b):
-            return (None, None, None, None)
+        if not (needs_grad_A_val or needs_grad_b):
+            return (None,) * 5
 
         A_coo_idx, x = ctx.saved_tensors
 
@@ -87,71 +84,67 @@ class _CuPySuperLUWrapper(t.autograd.Function):
         )
         lambda_ = t.from_dlpack(lambda_cp)
 
-        if needs_grad_b and not needs_grad_A:
-            return (None, None, lambda_, None)
+        if needs_grad_b and not needs_grad_A_val:
+            return (None, None, None, lambda_, None)
 
         # dLdA will have the same sparsity pattern as A
         dLdA_val = -lambda_[A_coo_idx[0]] * x[A_coo_idx[1]]
 
-        # The gradient for A will be in the uncoalesced COO format.
-        dLdA = t.sparse_coo_tensor(A_coo_idx, dLdA_val, ctx.A_shape)
+        if needs_grad_A_val and not needs_grad_b:
+            return (dLdA_val, None, None, None, None)
 
-        if needs_grad_A and not needs_grad_b:
-            return (dLdA, None, None, None)
-
-        if needs_grad_A and needs_grad_b:
-            return (dLdA, None, lambda_, None)
-
-
-def _csc_torch_to_scipy(torch_sp_csc: t.Tensor) -> scipy.sparse.csc_array:
-    src = torch_sp_csc.detach().cpu()
-
-    shape = tuple(src.shape)
-
-    ccol_idx = src.ccol_indices().numpy()
-    row_idx = src.row_indices().numpy()
-
-    val = src.values().numpy()
-
-    scipy_sp_csc = scipy.sparse.csc_array((val, row_idx, ccol_idx), shape=shape)
-
-    return scipy_sp_csc
+        if needs_grad_A_val and needs_grad_b:
+            return (dLdA_val, None, None, lambda_, None)
 
 
 class _SciPySuperLUWrapper(t.autograd.Function):
     @staticmethod
     def forward(
-        A: Float[t.Tensor, "r c"],
+        A_val: Float[t.Tensor, " nnz"],
         A_coo_idx: Integer[t.LongTensor, "2 nnz"],
+        A_shape: tuple[int, int],
         b: Float[t.Tensor, " r"],
         splu_kwargs: dict[str, Any],
-    ) -> tuple[Float[t.Tensor, " c"], cp_sp_linalg.SuperLU]:
-        A_scipy = _csc_torch_to_scipy(A)
+    ) -> tuple[Float[t.Tensor, " c"], scipy.sparse.linalg.SuperLU]:
+        A_scipy: Float[scipy.sparse.csc_array, "r c"] = scipy.sparse.coo_array(
+            (
+                A_val.detach().cpu().numpy(),
+                A_coo_idx.detach().cpu().numpy(),
+            ),
+            shape=A_shape,
+        ).tocsc()
         solver = scipy.sparse.linalg.splu(A_scipy, **splu_kwargs)
         x = t.from_numpy(solver.solve(b.detach().cpu().numpy(), trans="N")).to(
-            dtype=A.dtype, device=A.device
+            dtype=A_val.dtype, device=A_val.device
         )
         return x, solver
 
     @staticmethod
     def setup_context(ctx, inputs, output):
-        A, A_coo_idx, b, splu_kwargs = inputs
+        A_val, A_coo_idx, A_shape, b, splu_kwargs = inputs
 
         x, solver = output
         ctx.mark_non_differentiable(solver)
 
         ctx.save_for_backward(A_coo_idx, x)
         ctx.solver = solver
-        ctx.A_shape = tuple(A.shape)
+        ctx.A_shape = A_shape
 
     @staticmethod
     def backward(
         ctx, dLdx: Float[t.Tensor, " c"], _
-    ) -> tuple[Float[t.Tensor, "r c"] | None, None, Float[t.Tensor, " r"] | None, None]:
-        needs_grad_A, _, needs_grad_b, _ = ctx.needs_input_grad
+    ) -> tuple[
+        Float[t.Tensor, " nnz"] | None,
+        None,
+        None,
+        Float[t.Tensor, " r"] | None,
+        None,
+    ]:
+        needs_grad_A_val = ctx.needs_input_grad[0]
+        needs_grad_b = ctx.needs_input_grad[-2]
 
-        if not (needs_grad_A or needs_grad_b):
-            return (None, None, None, None)
+        if not (needs_grad_A_val or needs_grad_b):
+            return (None,) * 5
 
         A_coo_idx, x = ctx.saved_tensors
 
@@ -160,25 +153,21 @@ class _SciPySuperLUWrapper(t.autograd.Function):
         )
         lambda_ = t.from_numpy(lambda_scipy).to(dtype=dLdx.dtype, device=dLdx.device)
 
-        if needs_grad_b and not needs_grad_A:
-            return (None, None, lambda_, None)
+        if needs_grad_b and not needs_grad_A_val:
+            return (None, None, None, lambda_, None)
 
         # dLdA will have the same sparsity pattern as A
         dLdA_val = -lambda_[A_coo_idx[0]] * x[A_coo_idx[1]]
 
-        # The gradient for A will be in the uncoalesced COO format.
-        dLdA = t.sparse_coo_tensor(A_coo_idx, dLdA_val, ctx.A_shape)
+        if needs_grad_A_val and not needs_grad_b:
+            return (dLdA_val, None, None, None, None)
 
-        if needs_grad_A and not needs_grad_b:
-            return (dLdA, None, None, None)
-
-        if needs_grad_A and needs_grad_b:
-            return (dLdA, None, lambda_, None)
+        if needs_grad_A_val and needs_grad_b:
+            return (dLdA_val, None, None, lambda_, None)
 
 
 def solve(
     A: Float[t.Tensor, "r c"],
-    A_coo_idx: Integer[t.LongTensor, "2 nnz"],
     b: Float[t.Tensor, " r"],
     method: Literal["splu_cupy", "splu_scipy", "nvmath", "cg", "minres"],
     **kwargs,
@@ -186,28 +175,28 @@ def solve(
     """
     This function provides a differentiable wrapper for sparse linear solvers.
 
-    Here, A is a sparse tensor/matrix and b is a dense, 1D tensor. The A_coo_idx
-    is required for the backward pass to enforce the sparsity pattern of A.
+    Here, A is a sparse coo tensor and b is a dense, 1D tensor.
 
     If method is 'splu_cupy', solve the linear system using CuPy's SuperLU wrapper
-    This requires that `A` and `b` must be on the CUDA device and that `A` is
-    already in sparse CSC format. Note that the factorization step itself is not
-    accelerated on GPU, which necessitates data transfer with the host.
+    This requires that `A` and `b` must be on the CUDA device. Note that the
+    factorization step itself is not accelerated on GPU, which necessitates data
+    transfer with the host.
 
     If method is 'splu_scipy', solve the linear system using SciPy's SuperLU
-    wrapper. This requires that `A` is already in sparse CSC format; `A` and `b`
-    will be copied to CPU.
+    wrapper. This requires that `A` is in the sparse COO format (which is converted
+    to sparse CSC format in SciPy for the solver); `A` and `b` will be copied to CPU.
     """
     match method:
         case "splu_cupy":
             if not _HAS_CUPY:
                 raise ImportError("CuPy backend required for method 'splu_cupy'.")
 
-            x, solver = _CuPySuperLUWrapper.apply(A, A_coo_idx, b, kwargs)
+            x, solver = _CuPySuperLUWrapper.apply(A.values(), A.indices(), b, kwargs)
             return x
 
         case "splu_scipy":
-            x, solver = _SciPySuperLUWrapper.apply(A, A_coo_idx, b, kwargs)
+            x, solver = _SciPySuperLUWrapper.apply(A.values(), A.indices(), b, kwargs)
+            return x
 
         case _:
             raise NotImplementedError()
