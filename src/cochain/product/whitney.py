0580c4ba2c1f1015124785a072ba1ed15ed6a7e7
Lu Hong <honglu88@gmail.com>
2025-12-28 21:25:29 -0800

    further refactor of whitney helper functions

--- src/cochain/product/whitney.py
+++ src/cochain/product/whitney.py
@@ -13,20 +13,23 @@ from ..utils.perm_parity import compute_lex_rel_orient
 from ..utils.search import simplex_search
 
 
-def enumerate_faces(simp_dim: int, face_dim: int) -> Integer[t.Tensor, "face vert"]:
+def _enumerate_faces(
+    simp_dim: int, face_dim: int, device: t.device
+) -> Integer[t.Tensor, "face vert"]:
     """
-    For a simplex of dimension `simp_dim`, enumerate all canonical faces of dimension
-    `face_dim`.
+    For a simplex of dimension `simp_dim`, enumerate all faces of dimension
+    `face_dim` (up to vertex index permutation) in lex order.
     """
     if face_dim > simp_dim:
         raise ValueError()
 
     return t.tensor(
-        list(itertools.combinations(list(range(simp_dim + 1)), face_dim + 1))
+        list(itertools.combinations(list(range(simp_dim + 1)), face_dim + 1)),
+        device=device,
     )
 
 
-def compute_face_sign(
+def _compute_face_sign(
     mesh: SimplicialComplex, simp_dim: int, face_dim: int
 ) -> Float[t.Tensor, "simp face 1"]:
     """
@@ -39,7 +42,7 @@ def compute_face_sign(
         for dim, simp in enumerate([mesh.verts, mesh.edges, mesh.tris, mesh.tets])
     }
 
-    face_idx = enumerate_faces(simp_dim, face_dim)
+    face_idx = _enumerate_faces(simp_dim, face_dim, device=mesh.vert_coords.device)
     all_faces = simp_map[simp_dim][:, face_idx]
 
     signs = compute_lex_rel_orient(all_faces.flatten(end_dim=-2))
@@ -48,27 +51,27 @@ def compute_face_sign(
     return signs_shaped
 
 
-def compute_whitney_router(
+def _compute_whitney_router(
     simp_dim: int, form_deg: int, device: t.device, dtype: t.dtype = t.float
 ) -> Float[t.Tensor, "face lambda *d_lambda"]:
     """
     Compute the coefficients required to construct the Whitney forms from the
     λ's and the dλ's.
     """
-    faces = enumerate_faces(simp_dim, form_deg)
+    faces = _enumerate_faces(simp_dim, form_deg).detach().cpu().tolist()
 
-    router_shape = (faces.size(0),) + (simp_dim + 1,) * (form_deg + 1)
+    router_shape = (len(faces),) + (simp_dim + 1,) * (form_deg + 1)
     router = t.zeros(router_shape, dtype=dtype, device=device)
 
     for simp_idx, simp in enumerate(faces):
-        perms = t.tensor(list(itertools.permutations(simp)))
+        perms = t.tensor(list(itertools.permutations(simp)), dtype=dtype, device=device)
         signs = compute_lex_rel_orient(perms).to(dtype=dtype, device=device)
         router[simp_idx][perms.T.unbind(0)] = signs
 
     return router
 
 
-def compute_moments(
+def _compute_moments(
     order: int, simp_dim: int, device: t.device, dtype: t.dtype = t.float
 ) -> t.Tensor:
     """
@@ -77,7 +80,8 @@ def compute_moments(
 
     int[prod_i[λ_i^m_i]dV] = (n! * prod_i[m_i!]) / (n + sum_i[m_i])!
 
-    to compute the moment tensors.
+    to compute the moment tensors. The output tensor is of shape
+    (simp_dim + 1,) * order.
     """
     verts = list(range(simp_dim + 1))
     moments = t.zeros((len(verts),) * order)
@@ -93,37 +97,89 @@ def compute_moments(
     return moments.to(device=device, dtype=dtype)
 
 
-def inv_gram_det(g: Float[t.Tensor, "simp vert vert"], form_deg=int):
-    match form_deg:
-        case 0:
-            grad_wedge_dot = t.ones(g.shape[0], device=g.device)
-
-        case 1:
-            grad_wedge_dot = g
-
+def _compute_bc_grad_dot(mesh: SimplicialComplex) -> Float[t.Tensor, "simp vert vert"]:
+    """
+    A wrapper function for dispatching the correct bary_coord_grad_inner_prods()
+    function for either tri or tet meshes.
+    """
+    match mesh.dim:
         case 2:
-            n_simp = g.size(0)
-            n_vert = g.size(-1)
-            grad_wedge_dot = t.zeros(
+            abs_simp_size = tri_geometry.compute_tri_areas(mesh.vert_coords, mesh.tris)
+            abs_simp_size_grad = tri_geometry.compute_d_tri_areas_d_vert_coords(
+                mesh.vert_coords, mesh.tris
+            )
+            bc_grad_dot = tri_geometry.bary_coord_grad_inner_prods(
+                abs_simp_size, abs_simp_size_grad
+            )
+
+        case 3:
+            simp_size = tet_geometry.get_tet_signed_vols(mesh.vert_coords, mesh.tets)
+            abs_simp_size = t.abs(simp_size)
+            simp_size_grad = tet_geometry.d_tet_signed_vols_d_vert_coords(
+                mesh.vert_coords, mesh.tets
+            )
+            bc_grad_dot = tet_geometry.bary_coord_grad_inner_prods(
+                simp_size, simp_size_grad
+            )
+
+        case _:
+            raise NotImplementedError()
+
+    return bc_grad_dot
+
+
+def _inv_metric_det(
+    bc_grad_dot: Float[t.Tensor, "simp vert vert"], form_deg: int
+) -> Float[t.Tensor, " simp *d_lambda"]:
+    """
+    Compute the scalar inner products between wedge products of dλ's, which is
+    equivalent to computing the determinant of the inner products of the gradients
+    of λ's.
+    """
+    match form_deg:
+        # Because Whitney 0-forms do not involve dλ terms, return 1 for each simplex.
+        case 0:
+            d_bc_wedge_dot = t.ones(
+                bc_grad_dot.shape[0], dtype=bc_grad_dot.dtype, device=bc_grad_dot.device
+            )
+
+        # <dλ_i, dλ_j> = <grad[λ_i], grad[λ_j]>
+        case 1:
+            d_bc_wedge_dot = bc_grad_dot
+
+        # <dλ_i ⋀ dλ_j, dλ_k ⋀ dλ_l> is equal to the determinant of
+        #
+        # | <grad[λ_i], grad[λ_k]> <grad[λ_i], grad[λ_l]> |
+        # | <grad[λ_j], grad[λ_k]> <grad[λ_j], grad[λ_l]> |
+        case 2:
+            n_simp = bc_grad_dot.size(0)
+            n_vert = bc_grad_dot.size(-1)
+
+            d_bc_wedge_dot = t.zeros(
                 n_simp,
                 n_vert,
                 n_vert,
                 n_vert,
                 n_vert,
-                dtype=g.dtype,
-                device=g.device,
+                dtype=bc_grad_dot.dtype,
+                device=bc_grad_dot.device,
             )
 
-            # det_ijkl = g_ik*g_jl - g_il*g_jk
-
-            grad_wedge_dot.add_(t.einsum("tik,tjl->tijkl", g, g))
-            grad_wedge_dot.sub_(t.einsum("til,tjk->tijkl", g, g))
+            d_bc_wedge_dot.add_(t.einsum("tik,tjl->tijkl", bc_grad_dot, bc_grad_dot))
+            d_bc_wedge_dot.sub_(t.einsum("til,tjk->tijkl", bc_grad_dot, bc_grad_dot))
 
         # TODO: memory optimization
+
+        # <dλ_i ⋀ dλ_j ⋀ dλ_k, dλ_a ⋀ dλ_b ⋀ dλ_c> is equal to the determinant of
+        #
+        # | <grad[λ_i], grad[λ_a]> <grad[λ_i], grad[λ_b]> <grad[λ_i], grad[λ_c]> |
+        # | <grad[λ_j], grad[λ_a]> <grad[λ_j], grad[λ_b]> <grad[λ_j], grad[λ_c]> |
+        # | <grad[λ_k], grad[λ_a]> <grad[λ_k], grad[λ_b]> <grad[λ_k], grad[λ_c]> |
         case 3:
-            n_simp = g.size(0)
-            n_vert = g.size(-1)
-            grad_wedge_dot = t.zeros(
+            n_simp = bc_grad_dot.size(0)
+            n_vert = bc_grad_dot.size(-1)
+
+            d_bc_wedge_dot = t.zeros(
                 n_simp,
                 n_vert,
                 n_vert,
@@ -131,27 +187,43 @@ def inv_gram_det(g: Float[t.Tensor, "simp vert vert"], form_deg=int):
                 n_vert,
                 n_vert,
                 n_vert,
-                dtype=g.dtype,
-                device=g.device,
+                dtype=bc_grad_dot.dtype,
+                device=bc_grad_dot.device,
             )
 
-            # Calculate all permutations of the product g_ia*g_jb*g_kc
-            # Det = (123) - (132) - (213) + (231) + (312) - (321)
-            # | g_ir g_is g_it |
-            # | g_jr g_js g_jt |
-            # | g_kr g_ks g_kt |
+            # Denote <grad[λ_i], grad[λ_a]> as g_ia. Computing the determinant
+            # is equivalent to summing over all permutations of all products of
+            # the form sign(xyz)*g_ix*g_jy*gkz where x, y, z denotes permutations
+            # of a, b, c and the sign(xyz) denotes the parity of the permutation
+            # to arnage x, y, z back to lex order.
+            for x, y, z in ["abc", "bca", "cab"]:
+                d_bc_wedge_dot.add_(
+                    t.einsum(
+                        f"ti{x},tj{y},tk{z}->tijkabc",
+                        bc_grad_dot,
+                        bc_grad_dot,
+                        bc_grad_dot,
+                    )
+                )
 
-            grad_wedge_dot.add_(t.einsum("tia, tjb, tkc -> tijkabc", g, g, g))
-            grad_wedge_dot.sub_(t.einsum("tia, tjc, tkb -> tijkabc", g, g, g))
-            grad_wedge_dot.sub_(t.einsum("tib, tja, tkc -> tijkabc", g, g, g))
-            grad_wedge_dot.add_(t.einsum("tib, tjc, tka -> tijkabc", g, g, g))
-            grad_wedge_dot.add_(t.einsum("tic, tja, tkb -> tijkabc", g, g, g))
-            grad_wedge_dot.sub_(t.einsum("tic, tjb, tka -> tijkabc", g, g, g))
+            for x, y, z in ["acb", "bac", "cba"]:
+                d_bc_wedge_dot.sub_(
+                    t.einsum(
+                        f"ti{x},tj{y},tk{z}->tijkabc",
+                        bc_grad_dot,
+                        bc_grad_dot,
+                        bc_grad_dot,
+                    )
+                )
 
-    return grad_wedge_dot
+    return d_bc_wedge_dot
 
 
-def get_triple_scalar_prod_einsum_str(k: int, l: int) -> str:
+def _get_triple_tensor_prod_einsum_str(k: int, l: int) -> str:
+    """
+    Generate the string used in einsum() to compute the triple tensor product
+    from constituent components.
+    """
     m = k + l
     d_lambda_input_vars = "xyz"
     d_lambda_output_vars = "pqr"
@@ -176,55 +248,39 @@ def get_triple_scalar_prod_einsum_str(k: int, l: int) -> str:
     return einsum_str
 
 
-def triple_scalar_prod(
+def triple_tensor_prod(
     k: int,
     l: int,
     mesh: SimplicialComplex,
-):
-    sc_dim = mesh.dim
+) -> Float[t.Tensor, ""]:
+    """
+    Compute the triple product tensor T_ijk required for computing the load vector.
 
+    T_uvw is defined as the L^2 inner product <ϕ_u ⋀ ϕ_v, ϕ_w>, where ϕ_u is the
+    Whitney k-form defined on the k-simplex u, ϕ_v is the Whitney l-form defined
+    on the l-simplex v, and ϕ_w is the Whitney (k+l)-form defined on the
+    (k+l)-simplex w.
+    """
     device = mesh.vert_coords.device
-    float_dtype = mesh.vert_coords.dtype
-    int_dtype = mesh.edges.dtype
+    dtype = mesh.vert_coords.dtype
 
-    k_form_router = compute_whitney_router(sc_dim, k, device, float_dtype)
-    l_form_router = compute_whitney_router(sc_dim, l, device, float_dtype)
-    kl_form_router = compute_whitney_router(sc_dim, k + l, device, float_dtype)
+    k_form_router = _compute_whitney_router(mesh.dim, k, device, dtype)
+    l_form_router = _compute_whitney_router(mesh.dim, l, device, dtype)
+    kl_form_router = _compute_whitney_router(mesh.dim, k + l, device, dtype)
 
-    moments = compute_moments(3, sc_dim, device, float_dtype)
+    moments = _compute_moments(3, mesh.dim, device, dtype)
 
-    match sc_dim:
-        case 2:
-            abs_simp_size = tri_geometry.compute_tri_areas(mesh.vert_coords, mesh.tris)
-            abs_simp_size_grad = tri_geometry.compute_d_tri_areas_d_vert_coords(
-                mesh.vert_coords, mesh.tris
-            )
-            bary_cord_grad = tri_geometry.bary_coord_grad_inner_prods(
-                abs_simp_size, abs_simp_size_grad
-            )
+    bc_grad_dot = _compute_bc_grad_dot(mesh)
+    wedge_dot = _inv_metric_det(bc_grad_dot, k + l)
 
-        case 3:
-            simp_size = tet_geometry.get_tet_signed_vols(mesh.vert_coords, mesh.tets)
-            abs_simp_size = t.abs(simp_size)
-            simp_size_grad = tet_geometry.d_tet_signed_vols_d_vert_coords(
-                mesh.vert_coords, mesh.tets
-            )
-            wedge_dot = tet_geometry.bary_coord_grad_inner_prods(
-                simp_size, simp_size_grad
-            )
-        case _:
-            raise NotImplementedError()
-
-    wedge_dot = inv_gram_det(bary_cord_grad, k + l)
-
-    einsum_str = get_triple_scalar_prod_einsum_str(k, l)
+    einsum_str = _get_triple_tensor_prod_einsum_str(k, l)
 
     return t.einsum(
         einsum_str,
         k_form_router,
         l_form_router,
         kl_form_router,
-        abs_simp_size,
+        bc_grad_dot,
         moments,
         wedge_dot,
     )
@@ -240,7 +296,7 @@ def find_face_global_idx(
 
     # this is not necessary unless k is the top dimension in the complex.
     k_simp = simp_map[k].sort(dim=-1).values
-    all_k_faces = enumerate_faces(mesh_dim, k)
+    all_k_faces = _enumerate_faces(mesh_dim, k)
     k_face = simp_map[mesh_dim][:, all_k_faces]
     k_face_flat = k_face.view(-1, k + 1)
 
@@ -267,7 +323,6 @@ def whitney_wedge_product(
     pairing: Literal["scalar", "dot", "cross", "outer"],
 ) -> Float[t.Tensor, " (k+l)_simp ch"]:
     dtype = mesh.edges.dtype
-    pack_dtype = t.int64
     device = mesh.edges.device
 
     m = k + l
@@ -291,21 +346,21 @@ def whitney_wedge_product(
 
     # Do all k-faces
     k_face_idx, n_k_face = find_face_global_idx(k, mesh.dim, simp_map)
-    signed_k_cochain_at_k_face = compute_face_sign(mesh, mesh.dim, k) * k_cochain[
+    signed_k_cochain_at_k_face = _compute_face_sign(mesh, mesh.dim, k) * k_cochain[
         k_face_idx
     ].view(n_simp_map[mesh.dim], n_k_face, -1)
 
     # Do all l-faces
     l_face_idx, n_l_face = find_face_global_idx(l, mesh.dim, simp_map)
-    signed_l_cochain_at_l_face = compute_face_sign(mesh, mesh.dim, l) * l_cochain[
+    signed_l_cochain_at_l_face = _compute_face_sign(mesh, mesh.dim, l) * l_cochain[
         l_face_idx
     ].view(n_simp_map[mesh.dim], n_l_face, -1)
 
     # Do all m-faces
     m_face_idx, n_m_face = find_face_global_idx(m, mesh.dim, simp_map)
-    m_sign = compute_face_sign(mesh, mesh.dim, m)
+    m_sign = _compute_face_sign(mesh, mesh.dim, m)
 
-    load = triple_scalar_prod(k, l, mesh)
+    load = triple_tensor_prod(k, l, mesh)
 
     match pairing:
         case "scalar":
