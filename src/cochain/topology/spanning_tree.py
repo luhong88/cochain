0a48ac97d63dafe0b1cba8071b567c33c6112a52
Lu Hong <honglu88@gmail.com>
2026-02-26 21:56:27 -0800

    exclusion mask for spanning tree

--- src/cochain/topology/spanning_tree.py
+++ src/cochain/topology/spanning_tree.py
@@ -10,28 +10,46 @@ from cochain.sparse.operators import SparseOperator
 def _minimum_spanning_tree(
     adjacency: Float[SparseOperator, "node node"],
     root_mask: Bool[t.Tensor, " node"] | None = None,
-    weights: Float[t.Tensor, " node"] | None = None,
+    exclusion_mask: Bool[t.Tensor, " edge"] | None = None,
+    weights: Float[t.Tensor, " edge"] | None = None,
 ) -> Integer[t.LongTensor, "2 mst_node"]:
     """
     It is assumed that the nonzero elements in the input adjacency matrix are all
     strictly positive.
+
+    the root_mask is a boolean mask that specifies the node(s) that will serve as
+    the root(s) of the spanning tree/forest.
+
+    The exclusion_mask is a boolean mask whose elements correspond to the nonzero
+    elements of the adjacency matrix; an edge in the adjacency matrix marked as
+    True by the exclusion_mask is disallowed in constructing the spanning tree/forest.
     """
     n_nodes = adjacency.shape[0]
 
     # int32 is required for scipy sparse array indices
-    idx_coo = adjacency.sp_topo.idx_coo.to(dtype=t.int32)
-    coo_idx_rows = idx_coo[0].detach().cpu().numpy()
-    coo_idx_cols = idx_coo[1].detach().cpu().numpy()
+    idx_coo_full = adjacency.sp_topo.idx_coo.to(dtype=t.int32)
+    idx_coo_rows_full = idx_coo_full[0].detach().cpu().numpy()
+    idx_coo_cols_full = idx_coo_full[1].detach().cpu().numpy()
 
     # If provided, the weights overwrite the adjacency matrix data.
-    coo_data = (
+    coo_data_full = (
         adjacency.val.detach().cpu().numpy()
         if weights is None
         else weights.detach().cpu().numpy()
     )
 
+    # If provided, use the exclusion mask to remove banned edges.
+    if exclusion_mask is None:
+        idx_coo_rows = idx_coo_rows_full
+        idx_coo_cols = idx_coo_cols_full
+        coo_data = coo_data_full
+    else:
+        idx_coo_rows = idx_coo_rows_full[~exclusion_mask]
+        idx_coo_cols = idx_coo_cols_full[~exclusion_mask]
+        coo_data = coo_data_full[~exclusion_mask]
+
     if (root_mask is not None) and (root_mask.any()):
-        idx_dtype = coo_idx_rows.dtype
+        idx_dtype = idx_coo_rows.dtype
         data_dtype = coo_data.dtype
 
         # If the root(s) of the tree is specified, add a new "super node" to the
@@ -44,8 +62,8 @@ def _minimum_spanning_tree(
         new_cols = root_idx
         new_data = np.zeros(len(root_idx), dtype=data_dtype)
 
-        aug_rows = np.concatenate([coo_idx_rows, new_rows, new_cols])
-        aug_cols = np.concatenate([coo_idx_cols, new_cols, new_rows])
+        aug_rows = np.concatenate([idx_coo_rows, new_rows, new_cols])
+        aug_cols = np.concatenate([idx_coo_cols, new_cols, new_rows])
         aug_data = np.concatenate([coo_data, new_data, new_data])
 
         aug_shape = (n_nodes + 1, n_nodes + 1)
@@ -53,7 +71,7 @@ def _minimum_spanning_tree(
 
     else:
         aug_adjacency = coo_array(
-            (coo_data, (coo_idx_rows, coo_idx_cols)), shape=adjacency.shape
+            (coo_data, (idx_coo_rows, idx_coo_cols)), shape=adjacency.shape
         )
 
     mst = minimum_spanning_tree(aug_adjacency)
