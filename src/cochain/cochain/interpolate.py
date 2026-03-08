083df1d74ae39a34ae472a7755115e44354db522
Lu Hong <honglu88@gmail.com>
2026-03-07 20:29:55 -0800

    barycentric whitney interpolation for tri mesh

--- src/cochain/cochain/interpolate.py
+++ src/cochain/cochain/interpolate.py
@@ -5,19 +5,33 @@ from ..geometry.tet.tet_geometry import d_tet_signed_vols_d_vert_coords
 from ..geometry.tri.tri_geometry import compute_d_tri_areas_d_vert_coords
 
 
+def _bary_whitney_tri_cochain_0(
+    tris: Integer[t.LongTensor, "tri 3"],
+    cochain_0: Float[t.Tensor, " edge"],
+    bary_coords: Float[t.Tensor, "point 3"],
+) -> Float[t.Tensor, "tri point"]:
+    # W_i = λ_i for i = 0, 1, 2.
+    basis: Float[t.Tensor, "point vert=3"] = bary_coords
+
+    cochain_0_at_vert_faces: Float[t.Tensor, "tri vert=3"] = cochain_0[tris]
+
+    form_0: Float[t.Tensor, "tri point"] = t.einsum(
+        "pv,tv->tp", basis, cochain_0_at_vert_faces
+    )
+
+    return form_0
+
+
 def _bary_whitney_tri_cochain_1(
+    bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"],
     tri_edge_idx: Integer[t.LongTensor, "tri 3"],
     tri_edge_orientations: Float[t.Tensor, "tri 3"],
-    tri_areas: Float[t.Tensor, " tri 1 1"],
-    d_tri_areas_d_vert_coords: Float[t.Tensor, "tri 3 3"],
     cochain_1: Float[t.Tensor, " edge"],
     bary_coords: Float[t.Tensor, "point 3"],
-):
-    # The gradient of lambda_i(p) wrt p is given by grad_i(area_ijk)/area_ijk, a
-    # constant wrt p.
-    bary_coords_grad: Float[t.Tensor, "tri 1 vert=3 coord=3"] = (
-        d_tri_areas_d_vert_coords / tri_areas
-    ).view(-1, 1, 3, 3)
+) -> Float[t.Tensor, "tri point 3"]:
+    bary_coords_grad_shaped: Float[t.Tensor, "tri 1 vert=3 coord=3"] = (
+        bary_coords_grad.view(-1, 1, 3, 3)
+    )
 
     bary_coords_shaped: Float[t.Tensor, "1 point vert=3 1"] = bary_coords.view(
         1, -1, 3, 1
@@ -26,8 +40,10 @@ def _bary_whitney_tri_cochain_1(
     # W_ij = λ_i∇λ_j - λ_j∇λ_i for (i, j) = (0, 1), (0, 2), (1, 2)
     # Note that i, j switch positions for the second term.
     basis: Float[t.Tensor, "tri point edge=3 coord=3"] = (
-        bary_coords_shaped[:, :, [0, 0, 1]] * bary_coords_grad[:, :, [1, 2, 2], :]
-        - bary_coords_shaped[:, :, [1, 2, 2]] * bary_coords_grad[:, :, [0, 0, 1], :]
+        bary_coords_shaped[:, :, [0, 0, 1]]
+        * bary_coords_grad_shaped[:, :, [1, 2, 2], :]
+        - bary_coords_shaped[:, :, [1, 2, 2]]
+        * bary_coords_grad_shaped[:, :, [0, 0, 1], :]
     )
 
     # tri_edge_face_idx contains the index of edges 01, 02, and 12 in the list of
@@ -35,9 +51,30 @@ def _bary_whitney_tri_cochain_1(
     cochain_1_at_edge_faces: Float[t.Tensor, "tri edge=3"] = cochain_1[tri_edge_idx]
 
     # If the edges 01, 02, and 12 are not in their canonical orientation, then
-    # the corresponding basis function needs a sign correction given by tri_edge_orientations.
+    # the corresponding basis form needs a sign correction given by tri_edge_orientations.
     form_1: Float[t.Tensor, "tri point coord=3"] = t.einsum(
         "tpec,te,te->tpc", basis, tri_edge_orientations, cochain_1_at_edge_faces
     )
 
     return form_1
+
+
+def _bary_whitney_tri_cochain_2(
+    bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"],
+    tri_orientations: Float[t.Tensor, " tri"],
+    cochain_2: Float[t.Tensor, " tri"],
+) -> Float[t.Tensor, "tri point=1 3"]:
+    # There is only one basis form W_012 = 2(∇λ_1 x ∇λ_2); note that this basis
+    # function is a constant of barycentric coordinates, which means that the
+    # interpolated 2-forms will be constant on each triangle.
+    basis: Float[t.Tensor, "tri coord=3"] = t.cross(
+        bary_coords_grad[:, 1, :], bary_coords_grad[:, 2, :], dim=-1
+    )
+
+    # If the triangle is not in a canonical orientation, then the basis form
+    # needs a sign correction given by tri_orientations.
+    form_2: Float[t.Tensor, "tri point=1 coord=3"] = t.einsum(
+        "tc,t,t->tc", basis, tri_orientations, cochain_2
+    ).view(-1, 1, 3)
+
+    return form_2
