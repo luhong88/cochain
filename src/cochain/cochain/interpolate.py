97805d777db3a92ea17439acaa5b40a40dd538ee
Lu Hong <honglu88@gmail.com>
2026-03-16 23:01:39 -0700

    use einops in interpolate

--- src/cochain/cochain/interpolate.py
+++ src/cochain/cochain/interpolate.py
@@ -1,6 +1,7 @@
 from typing import Literal
 
 import torch as t
+from einops import einsum, rearrange, repeat
 from jaxtyping import Float, Integer
 
 from ..complex import SimplicialComplex
@@ -18,34 +19,31 @@ from ..utils.search import simplex_search
 
 def _bary_whitney_tri_cochain_0(
     cochain_0: Float[t.Tensor, " vert *ch"],
-    tris: Integer[t.LongTensor, "tri 3"],
-    bary_coords: Float[t.Tensor, "point 3"],
-) -> Float[t.Tensor, "tri point *ch coord=1"]:
+    tris: Integer[t.LongTensor, "tri vert=3"],
+    bary_coords: Float[t.Tensor, "pt vert=3"],
+) -> Float[t.Tensor, "tri pt *ch coord=1"]:
     # W_i = λ_i for i = 0, 1, 2.
-    basis: Float[t.Tensor, "point vert=3"] = bary_coords
+    basis: Float[t.Tensor, "pt vert=3"] = bary_coords
 
     cochain_0_at_vert_faces: Float[t.Tensor, "tri vert=3 *ch"] = cochain_0[tris]
 
-    form_0: Float[t.Tensor, "tri point *ch"] = t.einsum(
-        "pv,tv...->tp...", basis, cochain_0_at_vert_faces
+    form_0 = einsum(
+        "pt vert, tri vert ... -> tri pt ...", basis, cochain_0_at_vert_faces
     )
 
-    return form_0.unsqueeze(-1)
+    return rearrange(form_0, "tri pt ... -> tri pt ... 1")
 
 
 def _bary_whitney_tri_cochain_1(
     cochain_1: Float[t.Tensor, " edge *ch"],
-    tri_edge_idx: Integer[t.LongTensor, "tri 3"],
-    tri_edge_orientations: Float[t.Tensor, "tri 3"],
-    bary_coords: Float[t.Tensor, "point 3"],
+    tri_edge_idx: Integer[t.LongTensor, "tri vert=3"],
+    tri_edge_orientations: Float[t.Tensor, "tri vert=3"],
+    bary_coords: Float[t.Tensor, "pt vert=3"],
     bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"],
-) -> Float[t.Tensor, "tri point *ch coord=3"]:
-    bary_coords_grad_shaped: Float[t.Tensor, "tri 1 vert=3 coord=3"] = (
-        bary_coords_grad.view(-1, 1, 3, 3)
-    )
-
-    bary_coords_shaped: Float[t.Tensor, "1 point vert=3 1"] = bary_coords.view(
-        1, -1, 3, 1
+) -> Float[t.Tensor, "tri pt *ch coord=3"]:
+    bary_coords_shaped = rearrange(bary_coords, "pt coord -> 1 pt vert 1")
+    bary_coords_grad_shaped = rearrange(
+        bary_coords_grad, "tri vert coord -> tri 1 vert coord"
     )
 
     # W_ij = λ_i∇λ_j - λ_j∇λ_i for (i, j) = (0, 1), (0, 2), (1, 2)
@@ -53,7 +51,7 @@ def _bary_whitney_tri_cochain_1(
     local_edge_idx = enumerate_unique_faces(
         simp_dim=2, face_dim=1, device=bary_coords.device
     )
-    basis: Float[t.Tensor, "tri point edge=3 coord=3"] = (
+    basis: Float[t.Tensor, "tri pt edge=3 coord=3"] = (
         bary_coords_shaped[:, :, local_edge_idx[:, 0]]
         * bary_coords_grad_shaped[:, :, local_edge_idx[:, 1], :]
         - bary_coords_shaped[:, :, local_edge_idx[:, 1]]
@@ -66,8 +64,11 @@ def _bary_whitney_tri_cochain_1(
 
     # If the edges 01, 02, and 12 are not in their canonical orientation, then
     # the corresponding basis form needs a sign correction given by tri_edge_orientations.
-    form_1 = t.einsum(
-        "tpec,te,te...->tp...c", basis, tri_edge_orientations, cochain_1_at_edge_faces
+    form_1 = einsum(
+        "tri pt edge coord, tri edge, tri eedge ... -> tri pt ... coord",
+        basis,
+        tri_edge_orientations,
+        cochain_1_at_edge_faces,
     )
 
     return form_1
@@ -77,7 +78,7 @@ def _bary_whitney_tri_cochain_2(
     cochain_2: Float[t.Tensor, " tri *ch"],
     tri_orientations: Float[t.Tensor, " tri"],
     bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"],
-) -> Float[t.Tensor, "tri point=1 *ch coord=3"]:
+) -> Float[t.Tensor, "tri pt=1 *ch coord=3"]:
     # There is only one basis form W_012 = 2(∇λ_1 x ∇λ_2); note that this basis
     # function is a constant of barycentric coordinates, which means that the
     # interpolated 2-forms will be constant on each triangle.
@@ -87,43 +88,48 @@ def _bary_whitney_tri_cochain_2(
 
     # If the triangle is not in a canonical orientation, then the basis form
     # needs a sign correction given by tri_orientations.
-    form_2: Float[t.Tensor, "tri *ch coord=3"] = t.einsum(
-        "tc,t,t...->t...c", basis, tri_orientations, cochain_2
-    ).view(-1, 1, 3)
+    form_2 = (
+        einsum(
+            "tri coord, tri, tri ... -> tri ... coord",
+            basis,
+            tri_orientations,
+            cochain_2,
+        ),
+    )
 
-    return form_2.unsqueeze(1)
+    return rearrange(
+        form_2,
+        "tri ... coord -> tri 1 ... coord",
+    )
 
 
 def _bary_whitney_tet_cochain_0(
     cochain_0: Float[t.Tensor, " vert *ch"],
     tets: Integer[t.LongTensor, "tet 4"],
-    bary_coords: Float[t.Tensor, "point 4"],
-) -> Float[t.Tensor, "tet point *ch coord=1"]:
+    bary_coords: Float[t.Tensor, "pt 4"],
+) -> Float[t.Tensor, "tet pt *ch coord=1"]:
     # W_i = λ_i for i = 0, 1, 2, 3.
-    basis: Float[t.Tensor, "point vert=4"] = bary_coords
+    basis: Float[t.Tensor, "pt vert=4"] = bary_coords
 
     cochain_0_at_vert_faces: Float[t.Tensor, "tet vert=4 *ch"] = cochain_0[tets]
 
-    form_0: Float[t.Tensor, "tet *ch point"] = t.einsum(
-        "pv,tv...->tp...", basis, cochain_0_at_vert_faces
+    form_0 = einsum(
+        "pt vert, tet vert ... -> tet pt ...", basis, cochain_0_at_vert_faces
     )
 
-    return form_0.unsqueeze(-1)
+    return rearrange(form_0, "tet pt ... -> tet pt ... 1")
 
 
 def _bary_whitney_tet_cochain_1(
     cochain_1: Float[t.Tensor, " edge *ch"],
-    tet_edge_idx: Integer[t.LongTensor, "tet 6"],
-    tet_edge_orientations: Float[t.Tensor, "tet 6"],
-    bary_coords: Float[t.Tensor, "point 4"],
+    tet_edge_idx: Integer[t.LongTensor, "tet edge=6"],
+    tet_edge_orientations: Float[t.Tensor, "tet edge=6"],
+    bary_coords: Float[t.Tensor, "pt vert=4"],
     bary_coords_grad: Float[t.Tensor, "tet vert=4 coord=3"],
-) -> Float[t.Tensor, "tet point *ch coord=3"]:
-    bary_coords_grad_shaped: Float[t.Tensor, "tet 1 vert=4 coord=3"] = (
-        bary_coords_grad.view(-1, 1, 4, 3)
-    )
-
-    bary_coords_shaped: Float[t.Tensor, "1 point vert=4 1"] = bary_coords.view(
-        1, -1, 4, 1
+) -> Float[t.Tensor, "tet pt *ch coord=3"]:
+    bary_coords_shaped = rearrange(bary_coords, "pt vert -> 1 pt vert p1")
+    bary_coords_grad_shaped = rearrange(
+        bary_coords_grad, "tet vert coord -> tet 1 vert coord"
     )
 
     # W_ij = λ_i∇λ_j - λ_j∇λ_i for ij = 01, 02, 12, 13, 23, 03
@@ -131,7 +137,7 @@ def _bary_whitney_tet_cochain_1(
     local_edge_idx = enumerate_unique_faces(
         simp_dim=3, face_dim=1, device=bary_coords.device
     )
-    basis: Float[t.Tensor, "tet point edge=6 coord=3"] = (
+    basis: Float[t.Tensor, "tet pt edge=6 coord=3"] = (
         bary_coords_shaped[:, :, local_edge_idx[:, 0]]
         * bary_coords_grad_shaped[:, :, local_edge_idx[:, 1]]
         - bary_coords_shaped[:, :, local_edge_idx[:, 1]]
@@ -144,8 +150,11 @@ def _bary_whitney_tet_cochain_1(
 
     # If the edges are not in their canonical orientation, then the corresponding
     # basis form needs a sign correction given by tet_edge_orientations.
-    form_1 = t.einsum(
-        "tpec,te,te...->tp...c", basis, tet_edge_orientations, cochain_1_at_edge_faces
+    form_1 = einsum(
+        "tet pt edge coord, tet edge, tet edge ... -> tet pt ... coord",
+        basis,
+        tet_edge_orientations,
+        cochain_1_at_edge_faces,
     )
 
     return form_1
@@ -153,17 +162,14 @@ def _bary_whitney_tet_cochain_1(
 
 def _bary_whitney_tet_cochain_2(
     cochain_2: Float[t.Tensor, " tri *ch"],
-    tet_tri_idx: Integer[t.LongTensor, "tet 4"],
-    tet_tri_orientations: Float[t.Tensor, "tet 4"],
-    bary_coords: Float[t.Tensor, "point 4"],
+    tet_tri_idx: Integer[t.LongTensor, "tet tri=4"],
+    tet_tri_orientations: Float[t.Tensor, "tet tri=4"],
+    bary_coords: Float[t.Tensor, "pt vert=4"],
     bary_coords_grad: Float[t.Tensor, "tet vert=4 coord=3"],
-) -> Float[t.Tensor, "tet point *ch coord=3"]:
-    bary_coords_grad_shaped: Float[t.Tensor, "tet 1 vert=4 coord=3"] = (
-        bary_coords_grad.view(-1, 1, 4, 3)
-    )
-
-    bary_coords_shaped: Float[t.Tensor, "1 point vert=4 1"] = bary_coords.view(
-        1, -1, 4, 1
+) -> Float[t.Tensor, "tet pt *ch coord=3"]:
+    bary_coords_shaped = rearrange(bary_coords, "pt vert -> 1 pt vert 1")
+    bary_coords_grad_shaped = rearrange(
+        bary_coords_grad, "tet vert coord -> tet 1 vert coord"
     )
 
     # W_ijk = 2(λ_i ∇λ_jx∇λ_k + λ_j ∇λ_kx∇λ_i + λ_k ∇λ_ix∇ λ_j)
@@ -174,7 +180,7 @@ def _bary_whitney_tet_cochain_2(
     perm_i = local_tri_idx[:, 0]
     perm_j = local_tri_idx[:, 1]
     perm_k = local_tri_idx[:, 2]
-    basis: Float[t.Tensor, "tet point tri=4 coord=3"] = 2.0 * (
+    basis: Float[t.Tensor, "tet pt tri=4 coord=3"] = 2.0 * (
         bary_coords_shaped[:, :, perm_i]
         * t.cross(
             bary_coords_grad_shaped[:, :, perm_j],
@@ -202,7 +208,10 @@ def _bary_whitney_tet_cochain_2(
     # If the triangles are not in their canonical orientation, then the corresponding
     # basis form needs a sign correction given by tet_edge_orientations.
     form_2 = t.einsum(
-        "tpec,te,te...->tp...c", basis, tet_tri_orientations, cochain_2_at_tri_faces
+        "tet pt tri coord, tet tri, tet tri... -> tet pt ... coord",
+        basis,
+        tet_tri_orientations,
+        cochain_2_at_tri_faces,
     )
 
     return form_2
@@ -212,7 +221,7 @@ def _bary_whitney_tet_cochain_3(
     cochain_3: Float[t.Tensor, " tet *ch"],
     tet_signed_vols: Float[t.Tensor, " tet"],
     tet_orientations: Float[t.Tensor, " tet"],
-) -> Float[t.Tensor, "tet point=1 *ch coord=1"]:
+) -> Float[t.Tensor, "tet pt=1 *ch coord=1"]:
     # There is only one basis form W_0123 = 1/vol; note that this basis
     # function is a constant of barycentric coordinates, which means that the
     # interpolated 3-forms will be constant on each tet.
@@ -220,25 +229,25 @@ def _bary_whitney_tet_cochain_3(
 
     # If the tet is not in a canonical orientation, then the basis form
     # needs a sign correction given by tet_orientations.
-    form_3: Float[t.Tensor, " tet *ch"] = t.einsum(
-        "t,t,t...->t...", basis, tet_orientations, cochain_3
-    )
+    form_3 = einsum("tet, tet, tet ... -> tet ...", basis, tet_orientations, cochain_3)
 
-    return form_3.unsqueeze(1).unsqueeze(-1)
+    return rearrange(form_3, "tet 1 ... 1")
 
 
 def _bary_whitney_tri(
     k: int,
     k_cochain: Float[t.Tensor, " simp *ch"],
-    bary_coords: Float[t.Tensor, "point bary"],
+    bary_coords: Float[t.Tensor, "pt vert"],
     mesh: SimplicialComplex,
-) -> Float[t.Tensor, "tri point *ch coord"]:
+) -> Float[t.Tensor, "tri pt *ch coord"]:
     if k in [1, 2]:
-        tri_areas = compute_tri_areas(mesh.vert_coords, mesh.tris).view(-1, 1, 1)
+        tri_areas = rearrange(
+            compute_tri_areas(mesh.vert_coords, mesh.tris), "tri -> tri 1 1"
+        )
         d_tri_areas_d_vert_coords = compute_d_tri_areas_d_vert_coords(
             mesh.vert_coords, mesh.tris
         )
-        bary_coords_grad: Float[t.Tensor, "tri 3 3"] = (
+        bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"] = (
             d_tri_areas_d_vert_coords / tri_areas
         )
 
@@ -268,19 +277,19 @@ def _bary_whitney_tri(
 def _bary_whitney_tet(
     k: int,
     k_cochain: Float[t.Tensor, " simp *ch"],
-    bary_coords: Float[t.Tensor, "point coord"],
+    bary_coords: Float[t.Tensor, "pt vert"],
     mesh: SimplicialComplex,
-) -> Float[t.Tensor, "tet point *ch coord"]:
+) -> Float[t.Tensor, "tet pt *ch coord"]:
     if k in [1, 2, 3]:
-        tet_signed_vols = get_tet_signed_vols(mesh.vert_coords, mesh.tets).view(
-            -1, 1, 1
+        tet_signed_vols = rearrange(
+            get_tet_signed_vols(mesh.vert_coords, mesh.tets), "tet -> tet 1 1"
         )
 
     if k in [1, 2]:
         d_signed_vols_d_vert_coords = d_tet_signed_vols_d_vert_coords(
             mesh.vert_coords, mesh.tets
         )
-        bary_coords_grad: Float[t.Tensor, "tet 4 3"] = (
+        bary_coords_grad: Float[t.Tensor, "tet vert=4 coord=3"] = (
             d_signed_vols_d_vert_coords / tet_signed_vols
         )
 
@@ -317,9 +326,9 @@ def _bary_whitney_tet(
 
 def _bary_embed(
     m: int,
-    k_simps_bary_coords: Float[t.Tensor, "point coord"],
-    k_simps_local_vert_idx: Integer[t.LongTensor, "k_simp vert"],
-) -> Float[t.Tensor, "n_k_simp point m+1"]:
+    k_simps_bary_coords: Float[t.Tensor, "pt k_vert"],
+    k_simps_local_vert_idx: Integer[t.LongTensor, "k_simp k_vert"],
+) -> Float[t.Tensor, "k_simp pt m_vert"]:
     """
     Embed barycentric coordinates for a k-simplex onto the barycentric coordinates
     of the k-dimensional faces of a higher m-simplex.
@@ -366,8 +375,8 @@ def _bary_embed(
         dtype=dtype,
         device=device,
     )
-    src = k_simps_bary_coords.unsqueeze(0).expand(n_k_faces, -1, -1)
-    idx = k_simps_local_vert_idx.unsqueeze(1).expand(-1, n_pts, -1)
+    src = repeat(k_simps_bary_coords, "pt coord -> k_face pt coord", k_face=n_k_faces)
+    idx = repeat(k_simps_local_vert_idx, "k_simp vert -> k_simp pt vert", pt=n_pts)
 
     bary_coords_embedded.scatter_(dim=2, index=idx, src=src)
 
@@ -377,9 +386,9 @@ def _bary_embed(
 def _barycentric_whitney_map_interior(
     k: int,
     k_cochain: Float[t.Tensor, " simp *ch"],
-    bary_coords: Float[t.Tensor, "point coord"],
+    bary_coords: Float[t.Tensor, "pt vert"],
     mesh: SimplicialComplex,
-) -> Float[t.Tensor, "top_simp point *ch coord"]:
+) -> Float[t.Tensor, "top_simp pt *ch coord"]:
     match mesh.dim:
         case 2:
             return _bary_whitney_tri(k, k_cochain, bary_coords, mesh)
@@ -392,38 +401,47 @@ def _barycentric_whitney_map_interior(
 def _barycentric_whitney_map_boundary(
     k: int,
     k_cochain: Float[t.Tensor, " k_simp *ch"],
-    bary_coords: Float[t.Tensor, "point coord"],
+    bary_coords: Float[t.Tensor, "pt vert"],
     mesh: SimplicialComplex,
-) -> Float[t.Tensor, "k_simp point *ch coord"]:
-    local_face_idx = enumerate_unique_faces(
-        simp_dim=mesh.dim, face_dim=k, device=mesh.vert_coords.device
+) -> Float[t.Tensor, "k_simp pt *ch coord"]:
+    m = mesh.dim
+
+    local_face_idx: Integer[t.LongTensor, "k_face k_vert"] = enumerate_unique_faces(
+        simp_dim=m, face_dim=k, device=mesh.vert_coords.device
     )
 
     # Perform barycentric coordinate embedding and then compute the whitney
     # interpolation at the top simplex level.
-    bary_coords_embedded = _bary_embed(
-        m=mesh.dim,
-        k_simps_bary_coords=bary_coords,
-        k_simps_local_vert_idx=local_face_idx,
+    bary_coords_embedded: Float[t.Tensor, "k_face pt m_vert"] = _bary_embed(
+        m,
+        bary_coords,
+        local_face_idx,
     )
     n_k_faces, n_pts, _ = bary_coords_embedded.shape
 
     k_forms = _barycentric_whitney_map_interior(
-        k, k_cochain, bary_coords_embedded.view(-1, mesh.dim + 1), mesh
+        k,
+        k_cochain,
+        rearrange(bary_coords_embedded, "k_face pt m_vert -> (k_face pt) m_vert"),
+        mesh,
     )
-    k_forms_shaped: Float[t.Tensor, "top_simp*k_face point *ch coord"] = k_forms.view(
-        k_forms.shape[0] * n_k_faces, n_pts, *k_forms.shape[2:]
+    k_forms_shaped = rearrange(
+        k_forms,
+        "m_simp (k_face pt) ... coord -> (m_simp k_face) pt ... coord",
+        k_face=n_k_faces,
+        pt=n_pts,
     )
 
     # Find the global indices of all k-faces.
     # TODO: this is not ideal since we are repating topo calculations
-    all_faces: Integer[t.LongTensor, "top_simp*n_k_face k+1"] = mesh.simplices[
-        mesh.dim
-    ][:, local_face_idx].flatten(end_dim=-2)
+    all_faces = rearrange(
+        mesh.simplices[m][:, local_face_idx],
+        "m_simp k_face k_vert -> (m_simp k_face) k_vert",
+    )
 
-    global_face_idx: Integer[t.LongTensor, "top_simp*k_face point *ch coord"] = (
+    global_face_idx: Integer[t.LongTensor, "m_simp*k_face pt *ch coord"] = (
         simplex_search(
-            key_simps=mesh.simplices[mesh.dim],
+            key_simps=mesh.simplices[m],
             query_simps=all_faces,
             sort_key_simp=False,
             sort_key_vert=False,
@@ -445,7 +463,7 @@ def _barycentric_whitney_map_boundary(
     # normal components on the k-simplices, and 2) the second approach is not
     # safe for autograd since it doesn't correctly link each k-simplex to all of
     # its top-level cofaces.
-    canon_k_forms: Float[t.Tensor, "k_simp point *ch coord"] = t.zeros(
+    canon_k_forms: Float[t.Tensor, "k_simp pt *ch coord"] = t.zeros(
         (mesh.simplices[k].size(0), *k_forms_shaped.shape[1:]),
         dtype=k_cochain.dtype,
         device=k_cochain.device,
@@ -464,10 +482,10 @@ def _barycentric_whitney_map_boundary(
 def barycentric_whitney_map(
     k: int,
     k_cochain: Float[t.Tensor, " k_simp *ch"],
-    bary_coords: Float[t.Tensor, "point coord"],
+    bary_coords: Float[t.Tensor, "pt vert"],
     mesh: SimplicialComplex,
     mode: Literal["interior", "boundary"],
-) -> Float[t.Tensor, "simp point *ch coord"]:
+) -> Float[t.Tensor, "simp pt *ch coord"]:
     """
     This function implements an "element-local" version of the Whitney map for
     interpolating discrete k-cochains, which is useful for numerical quadrature.
