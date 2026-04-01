7df5e201e907c7188bed54a0c7d982f27097d494
Lu Hong <honglu88@gmail.com>
2026-03-31 22:20:56 -0700

    vol averaged vertex based sharp

--- src/cochain/cochain/music_ops/_geometric.py
+++ src/cochain/cochain/music_ops/_geometric.py
@@ -4,6 +4,7 @@ import torch as t
 from einops import einsum, rearrange, repeat
 from jaxtyping import Float, Integer
 
+from ...sparse.decoupled_tensor import DiagDecoupledTensor
 from ...utils.bary_coords import get_k_splx_barycenters, get_tri_circumcenters
 from ..interpolate import _bary_whitney_tet_cochain_1, _bary_whitney_tri_cochain_1
 
@@ -148,7 +149,7 @@ def element_based_tri_geometric_sharp(
         tri_edge_orientations=tri_edge_orientations,
         bary_coords=bary_coords,
         bary_coords_grad=bary_coords_grad,
-    ).squeeze()
+    ).squeeze(dim=1)
 
     return form_1
 
@@ -164,7 +165,7 @@ def element_based_tet_geometric_sharp(
     tets.
     """
     bary_coords = get_k_splx_barycenters(
-        k=2, dtype=cochain_1.dtype, device=cochain_1.device
+        k=3, dtype=cochain_1.dtype, device=cochain_1.device
     )
 
     form_1 = _bary_whitney_tet_cochain_1(
@@ -176,3 +177,121 @@ def element_based_tet_geometric_sharp(
     ).squeeze()
 
     return form_1
+
+
+def vertex_based_tri_geometric_sharp(
+    cochain_1: Float[t.Tensor, " edge"],
+    star_0: Float[DiagDecoupledTensor, "vert vert"],
+    n_verts: int,
+    tris: Integer[t.LongTensor, "tri vert=3"],
+    tri_edge_idx: Integer[t.LongTensor, "tri edge=3"],
+    tri_edge_orientations: Float[t.Tensor, "tri edge=3"],
+    tri_areas: Float[t.Tensor, " tri"],
+    vert_coords: Float[t.Tensor, "vert coord=3"],
+    bary_coords_grad: Float[t.Tensor, "tri vert=3 coord=3"],
+) -> Float[t.Tensor, "vert coord=3"]:
+    """
+    Compute the vertex-based sharp of a 1-cochain by first using the element-based
+    approach and then taking an area-weighted average of the 1-form over all triangles
+    sharing a given vertex.
+
+    Note that, since the Whitney 1-form basis functions are discontinuous across
+    the tris, the value of the interpolated 1-form from the element-based approach
+    is not well-defined at the vertices. Therefore, this area-weighted approach
+    does not satisfy the adjoint relation with the vertex-based geometric flat
+    operator and can introduce numerical artifacts.
+    """
+    n_coords = 3
+
+    # Interpolate the 1-cochain at the barycenter, which is equivalent to computing
+    # the average of the interpolated 1-form over the tris.
+    form_1_on_tris = element_based_tri_geometric_sharp(
+        cochain_1=cochain_1,
+        tris=tris,
+        tri_edge_idx=tri_edge_idx,
+        tri_edge_orientations=tri_edge_orientations,
+        vert_coords=vert_coords,
+        bary_coords_grad=bary_coords_grad,
+        location="barycenter",
+    )
+    area_weighted_form_1 = tri_areas.unsqueeze(-1) * form_1_on_tris
+
+    # Reshape tris and area_weighted_form_1 in preparation for scatter add.
+    tris_shaped = repeat(tris, "tri vert -> (tri vert) coord", coord=n_coords)
+    area_weighted_form_1_shaped = repeat(
+        area_weighted_form_1, "tri coord -> (tri vert) coord", vert=n_verts
+    )
+
+    area_weighted_form_1_on_verts = t.zeros(
+        (n_verts, n_coords), dtype=cochain_1.dtype, device=cochain_1.device
+    )
+
+    # self[idx[tri_by_vert][coord]][coord] += src[tri_by_vert][coord]
+    area_weighted_form_1_on_verts.scatter_add_(
+        dim=0,
+        index=tris_shaped,
+        src=area_weighted_form_1_shaped,
+    )
+
+    # The barycentric star-0 gives 1/3 of the total area of all triangles sharing
+    # each vert as a face.
+    area_weights = star_0.val.unsqueeze(-1) * 3.0
+
+    # Use the area-weighted average to assign the 1-form to vertices.
+    sharp = area_weighted_form_1_on_verts / area_weights
+
+    return sharp
+
+
+def vertex_based_tet_geometric_sharp(
+    cochain_1: Float[t.Tensor, " edge"],
+    star_0: Float[DiagDecoupledTensor, "vert vert"],
+    n_verts: int,
+    tets: Integer[t.LongTensor, "tet vert=4"],
+    tet_edge_idx: Integer[t.LongTensor, "tet local_edge=6"],
+    tet_edge_orientations: Float[t.Tensor, "tet local_edge=6"],
+    tet_unsigned_vols: Float[t.Tensor, " tet"],
+    bary_coords_grad: Float[t.Tensor, "tet vert=4 coord=3"],
+) -> Float[t.Tensor, "vert coord=3"]:
+    """
+    Compute the vertex-based sharp of a 1-cochain by first using the element-based
+    approach and then taking an area-weighted average of the 1-form over all tets
+    sharing a given vertex.
+    """
+    n_coords = 3
+
+    # Interpolate the 1-cochain at the barycenter, which is equivalent to computing
+    # the average of the interpolated 1-form over the tets.
+    form_1_on_tets = element_based_tet_geometric_sharp(
+        cochain_1=cochain_1,
+        tet_edge_idx=tet_edge_idx,
+        tet_edge_orientations=tet_edge_orientations,
+        bary_coords_grad=bary_coords_grad,
+    )
+    vol_weighted_form_1 = tet_unsigned_vols.unsqueeze(-1) * form_1_on_tets
+
+    # Reshape tets and vol_weighted_form_1 in preparation for scatter add.
+    tets_shaped = repeat(tets, "tet vert -> (tet vert) coord", coord=n_coords)
+    vol_weighted_form_1_shaped = repeat(
+        vol_weighted_form_1, "tet coord -> (tet vert) coord", vert=n_verts
+    )
+
+    vol_weighted_form_1_on_verts = t.zeros(
+        (n_verts, n_coords), dtype=cochain_1.dtype, device=cochain_1.device
+    )
+
+    # self[idx[tet_by_vert][coord]][coord] += src[tet_by_vert][coord]
+    vol_weighted_form_1_on_verts.scatter_add_(
+        dim=0,
+        index=tets_shaped,
+        src=vol_weighted_form_1_shaped,
+    )
+
+    # The barycentric star-0 gives 1/4 of the total volume of all tets sharing
+    # each vert as a face.
+    vol_weights = star_0.val.unsqueeze(-1) * 4.0
+
+    # Use the volume-weighted average to assign the 1-form to vertices.
+    sharp = vol_weighted_form_1_on_verts / vol_weights
+
+    return sharp
