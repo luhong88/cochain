a632c649bb9f3fd61b8b79a5cb9981840411c84b
Lu Hong <honglu88@gmail.com>
2026-04-04 16:37:33 -0700

    patch tests for galerkin_contract

---
+++ tests/cochain/test_int_prod.py
@@ -0,0 +1,169 @@
+import pytest
+import torch
+from einops import einsum, rearrange, repeat
+
+from cochain.cochain import music_ops
+from cochain.cochain.discretize import DeRhamMap
+from cochain.cochain.ext_prod.cup import AntisymmetricCupProduct
+from cochain.cochain.ext_prod.whitney import WhitneyWedgeL2Projector
+from cochain.cochain.int_prod import algebraic_contract, galerkin_contract
+from cochain.complex import SimplicialMesh
+from cochain.geometry.tet import tet_masses
+from cochain.geometry.tri import tri_masses
+
+
+def test_galerkin_contraction_1_form_on_tet_mesh(two_tets_mesh: SimplicialMesh, device):
+    """
+    Test that the interior product between a constant vector field and a constant
+    k-form can be exactly reproduced on a tet mesh using the Galerkin method.
+
+    Note that we do not test this property on tri meshes, because a random vector
+    field is in general not in the tangent space of a tri mesh. Since the
+    galerkin_contract() function is built on top of mass matrices and wedge
+    products that have already been tested for both tri and tet meshes, we avoid
+    the issue with tangent space here in these tests by using tet meshes only.
+    """
+    mesh = two_tets_mesh.to(device)
+
+    # Set up a constant vector field and a constant 1-form and compute their
+    # interior product analytically, which is simply the inner product.
+    const_vec = torch.randn(3, dtype=mesh.dtype, device=mesh.device)
+    const_1_form = torch.randn(3, dtype=mesh.dtype, device=mesh.device)
+
+    # Note that a constant 0-form is identical to its discretization, so there is
+    # no need to convert the 0-form to a 0-cochain.
+    int_prod_true = torch.sum(const_vec * const_1_form).view(1).expand(mesh.n_verts)
+
+    # Compute the flat of the constant vector field.
+    edge_verts = mesh.vert_coords[mesh.edges]
+    edge_vecs = edge_verts[:, 1] - edge_verts[:, 0]
+    const_vec_flat = einsum(const_vec, edge_vecs, "coord, edge coord -> edge")
+
+    # Compute the flat of the constant 1-form using the de Rham map.
+    de_rham_1 = DeRhamMap(k=1, quad_degree=1, mesh=mesh)
+
+    sampled_points = de_rham_1.sample_points()
+    n_edges, n_pts, _ = sampled_points.shape
+    const_1_form_shaped = repeat(
+        const_1_form, "coord -> edge pt coord", edge=n_edges, pt=n_pts
+    )
+
+    const_1_cochain = de_rham_1.discretize(const_1_form_shaped)
+
+    # Compute the interior product using the galerkin_contract() method.
+    wedge_op = WhitneyWedgeL2Projector(k=1, l=0, mesh=mesh)
+    mass_0 = tet_masses.mass_0(mesh)
+
+    int_prod = galerkin_contract(
+        cochain_k=const_1_cochain,
+        cochain_1=const_vec_flat,
+        mass_km1=mass_0,
+        wedge_op=wedge_op,
+        method="dense",
+    )
+
+    torch.testing.assert_close(int_prod, int_prod_true)
+
+
+def test_galerkin_contraction_2_form_on_tet_mesh(two_tets_mesh: SimplicialMesh, device):
+    mesh = two_tets_mesh.to(device)
+
+    # Set up a constant vector field and a constant 2-form and compute their
+    # interior product analytically, which is simply the cross product.
+    const_vec = torch.randn(3, dtype=mesh.dtype, device=mesh.device)
+    const_2_form = torch.randn(3, dtype=mesh.dtype, device=mesh.device)
+
+    # Note that, the interior product must be computed as ω x v, rather than v x ω.
+    int_prod_form = torch.cross(const_2_form, const_vec)
+
+    # Discretize the true interior product into a 1-cochain.
+    de_rham_1 = DeRhamMap(k=1, quad_degree=1, mesh=mesh)
+
+    sampled_points = de_rham_1.sample_points()
+    n_edges, n_pts, _ = sampled_points.shape
+    int_prod_form_shaped = repeat(
+        int_prod_form, "coord -> edge pt coord", edge=n_edges, pt=n_pts
+    )
+
+    int_prod_cochain_true = de_rham_1.discretize(int_prod_form_shaped)
+
+    # Compute the flat of the constant vector field.
+    edge_verts = mesh.vert_coords[mesh.edges]
+    edge_vecs = edge_verts[:, 1] - edge_verts[:, 0]
+    const_vec_flat = einsum(const_vec, edge_vecs, "coord, edge coord -> edge")
+
+    # Compute the flat of the constant 2-form using the de Rham map.
+    de_rham_2 = DeRhamMap(k=2, quad_degree=1, mesh=mesh)
+
+    sampled_points = de_rham_2.sample_points()
+    n_tris, n_pts, _ = sampled_points.shape
+    const_2_form_shaped = repeat(
+        const_2_form, "coord -> tri pt coord", tri=n_tris, pt=n_pts
+    )
+
+    const_2_cochain = de_rham_2.discretize(const_2_form_shaped)
+
+    # Compute the interior product using the galerkin_contract() method.
+    wedge_op = WhitneyWedgeL2Projector(k=1, l=1, mesh=mesh)
+    mass_1 = tet_masses.mass_1(mesh)
+
+    int_prod = galerkin_contract(
+        cochain_k=const_2_cochain,
+        cochain_1=const_vec_flat,
+        mass_km1=mass_1,
+        wedge_op=wedge_op,
+        method="dense",
+    )
+
+    torch.testing.assert_close(int_prod, int_prod_cochain_true)
+
+
+def test_galerkin_contraction_3_form_on_tet_mesh(two_tets_mesh: SimplicialMesh, device):
+    mesh = two_tets_mesh.to(device)
+
+    # Set up a constant vector field and a constant 3-form and compute their
+    # interior product analytically, which is simply the scalar product.
+    const_vec = torch.randn(3, dtype=mesh.dtype, device=mesh.device)
+    const_3_form = torch.randn(1, dtype=mesh.dtype, device=mesh.device)
+    int_prod_form = const_3_form * const_vec
+
+    # Discretize the true interior product into a 2-cochain.
+    de_rham_2 = DeRhamMap(k=2, quad_degree=1, mesh=mesh)
+
+    sampled_points = de_rham_2.sample_points()
+    n_tris, n_pts, _ = sampled_points.shape
+    int_prod_form_shaped = repeat(
+        int_prod_form, "coord -> tri pt coord", tri=n_tris, pt=n_pts
+    )
+
+    int_prod_cochain_true = de_rham_2.discretize(int_prod_form_shaped)
+
+    # Compute the flat of the constant vector field.
+    edge_verts = mesh.vert_coords[mesh.edges]
+    edge_vecs = edge_verts[:, 1] - edge_verts[:, 0]
+    const_vec_flat = einsum(const_vec, edge_vecs, "coord, edge coord -> edge")
+
+    # Compute the flat of the constant 3-form using the de Rham map.
+    de_rham_3 = DeRhamMap(k=3, quad_degree=1, mesh=mesh)
+
+    sampled_points = de_rham_3.sample_points()
+    n_tets, n_pts, _ = sampled_points.shape
+    const_3_form_shaped = repeat(
+        const_3_form, "coord -> tet pt coord", tet=n_tets, pt=n_pts
+    )
+
+    const_3_cochain = de_rham_3.discretize(const_3_form_shaped)
+
+    # Compute the interior product using the galerkin_contract() method.
+    wedge_op = WhitneyWedgeL2Projector(k=1, l=2, mesh=mesh)
+    mass_2 = tet_masses.mass_2(mesh)
+
+    int_prod = galerkin_contract(
+        cochain_k=const_3_cochain,
+        cochain_1=const_vec_flat,
+        mass_km1=mass_2,
+        wedge_op=wedge_op,
+        method="dense",
+    )
+
+    torch.testing.assert_close(int_prod, int_prod_cochain_true)
