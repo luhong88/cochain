04a8001411a4362d1c9efa6bbd35b9b770fa8b52
Lu Hong <honglu88@gmail.com>
2026-04-08 14:21:58 -0700

    update tri stiffness tests

--- tests/geometry/tri/test_tri_stiffness.py
+++ tests/geometry/tri/test_tri_stiffness.py
@@ -5,59 +5,79 @@ from cochain.complex import SimplicialMesh
 from cochain.geometry.tri.tri_stiffness import stiffness_matrix
 
 
-def test_stiffness_with_igl(two_tris_mesh: SimplicialMesh):
+def test_stiffness_with_igl(two_tris_mesh: SimplicialMesh, device):
     """
+    Check stiffness matrix correctness with igl.
+
     Validate the stiffness matrix calculation using the external library `libigl`,
     which performs the same calculation with the `cotmatrix()` function.
     """
+    mesh = two_tris_mesh.to(device)
+
     igl_cotan_laplacian = -torch.from_numpy(
         igl.cotmatrix(
-            two_tris_mesh.vert_coords.cpu().detach().numpy(),
-            two_tris_mesh.tris.cpu().detach().numpy(),
+            mesh.vert_coords.cpu().detach().numpy(),
+            mesh.tris.cpu().detach().numpy(),
         ).todense(),
-    ).to(dtype=torch.float32)
+    ).to(dtype=mesh.dtype, device=device)
 
-    cochain_cotan_laplacian = stiffness_matrix(two_tris_mesh).to_dense()
+    cotan_laplacian = stiffness_matrix(mesh).to_dense()
 
-    torch.testing.assert_close(igl_cotan_laplacian, cochain_cotan_laplacian)
+    torch.testing.assert_close(igl_cotan_laplacian, cotan_laplacian)
 
 
-def test_stiffness_kernel(icosphere_mesh: SimplicialMesh):
+def test_stiffness_kernel(icosphere_mesh: SimplicialMesh, device):
     """
+    Check that constant vectors are in the kernel of the stiffness matrix.
+
     The stiffness matrix acting on a constant function over the vertices should
     return the zero vector. This can be checked by comparing the row sum of the
     matrix with the zero vector.
     """
-    sphere_S = stiffness_matrix(icosphere_mesh)
+    mesh = icosphere_mesh.to(device)
+
+    sphere_S = stiffness_matrix(mesh)
     row_sum = sphere_S.to_dense().sum(dim=-1)
+
     torch.testing.assert_close(row_sum, torch.zeros_like(row_sum))
 
 
-def test_stiffness_symmetry(icosphere_mesh: SimplicialMesh):
-    """
-    The stifness matrix should be a symmetric matrix.
-    """
-    sphere_S = stiffness_matrix(icosphere_mesh)
+def test_stiffness_symmetry(icosphere_mesh: SimplicialMesh, device):
+    """Check that the stifness matrix is symmetric."""
+    mesh = icosphere_mesh.to(device)
+
+    sphere_S = stiffness_matrix(mesh)
     sphere_S_dense = sphere_S.to_dense()
+
     torch.testing.assert_close(sphere_S_dense, sphere_S_dense.T)
 
 
-def test_stiffness_PSD(icosphere_mesh: SimplicialMesh):
-    """
-    The stiffness matrix should be a positive semi-definite matrix.
-    """
-    sphere_S = stiffness_matrix(icosphere_mesh)
+def test_stiffness_PSD(icosphere_mesh: SimplicialMesh, device):
+    """Check that the stiffness matrix is positive semi-definite."""
+    mesh = icosphere_mesh.to(device)
+
+    sphere_S = stiffness_matrix(mesh)
     sphere_S_dense = sphere_S.to_dense()
     eigs = torch.linalg.eigvalsh(sphere_S_dense)
+
     assert eigs.min() >= -1e-6
 
 
-def test_stiffness_planar(square_mesh: SimplicialMesh):
+def test_stiffness_planar(flat_annulus_mesh: SimplicialMesh, device):
     """
-    The stiffness matrix acting on a planar mesh coordinates should result in
-    zero (for interior vertices).
-    """
-    plane_S = stiffness_matrix(square_mesh)
-    zero_tensor = plane_S @ square_mesh.vert_coords
+    Check the kernel of the stiffness matrix of a planar mesh.
 
-    torch.testing.assert_close(zero_tensor[-1], torch.zeros_like(zero_tensor[-1]))
+    The stiffness matrix acting on a planar mesh coordinates should result in
+    zero (for interior vertices). This is because the stiffness matrix acting on
+    the vertex coordinate vector gives the mean curvature normal vector.
+    """
+    mesh = flat_annulus_mesh.to(device)
+
+    plane_S = stiffness_matrix(mesh)
+    zero_tensor = plane_S @ mesh.vert_coords
+
+    bd_mask = mesh.bd_vert_mask
+
+    torch.testing.assert_close(
+        zero_tensor[~bd_mask], torch.zeros_like(zero_tensor[~bd_mask])
+    )
