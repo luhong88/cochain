b5bde24dec529a4603c710de3da89bf1efc29ce2
Lu Hong <honglu88@gmail.com>
2026-04-09 00:25:39 -0700

    docstrings for tet hodge stars

--- tests/metric/tet/test_tet_hodge_stars.py
+++ tests/metric/tet/test_tet_hodge_stars.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from cochain.complex import SimplicialMesh
@@ -18,3 +19,24 @@ def test_star_1_on_reg_tet(reg_tet_mesh: SimplicialMesh):
     true_s1 = torch.ones_like(s1) * (1.0 / 6.0)
 
     torch.testing.assert_close(s1, true_s1)
+
+
+@pytest.mark.parametrize(
+    "star_op",
+    [
+        tet_hodge_stars.star_0,
+        tet_hodge_stars.star_1,
+        tet_hodge_stars.star_2,
+        tet_hodge_stars.star_3,
+    ],
+)
+def test_star_backward(star_op, two_tets_mesh: SimplicialMesh, device):
+    mesh = two_tets_mesh.to(device)
+    mesh.requires_grad_()
+
+    star = star_op(mesh)
+    output = star.val.sum()
+    output.backward()
+
+    assert mesh.grad is not None
+    assert torch.isfinite(mesh.grad).all()
