4abda4f7f45dd43c3033d57c28f6de6288a841c7
Lu Hong <honglu88@gmail.com>
2026-05-17 07:14:53 -0700

    update local music op docstrings

--- src/cochain/cochain/music_ops/local.py
+++ src/cochain/cochain/music_ops/local.py
@@ -13,21 +13,37 @@ from . import _local_element, _local_vertex
 
 
 def local_flat(
-    vec_field: Float[Tensor, "splx coord"],
+    vec_field: Float[Tensor, "splx coord=3"],
     mesh: SimplicialMesh,
     mode: Literal["element", "vertex"],
 ) -> Float[Tensor, " edge"]:
     """
     Compute the flat of a vector field using a local, interpolation based method.
 
-    If `mode` is "element", the input vector field is assumed to be piecewise
-    constant and associated with the top-level simplices of the mesh, and the flat
-    is computed at each edge by taking the dot product between the edge vector
-    and the mean of the field across all cofaces of the edge. If `mode` is "vertex",
-    the input vector field is assumed to be associated with the vertices of the
-    mesh and the flat is computed at each edge as the dot product between the edge
-    vector and the mean of the field at the two end vertices of the edge.
+    Parameters
+    ----------
+    vec_field : [splx, coord]
+        The input vector field. The interpretation of the first `splx` dimension
+        depends on the `mode` argument. If `mode` is "element", the input vector
+        field is assumed to be discrete/piecewise constant and associated with
+        the top-level simplices of the mesh. If `mode` is "vertex", the input vector
+        field is assumed to be associated with the vertices of the mesh.
+    mesh
+        A simplicial mesh.
+    mode
+        How the flat is computed. If `mode` is "element`", the flat is computed
+        at each edge by taking the dot product between the edge vector and the
+        mean of the field across all cofaces of the edge; if `mode` is "vertex",
+        the flat is computed at each edge as the dot product between the edge
+        vector and the mean of the field at the two end vertices of the edge.
 
+    Returns
+    -------
+    [edge,]
+        A 1-cochain representing the flat of the input vector field.
+
+    Notes
+    -----
     Note that this local, interpolation based method is in general faster than
     the Galerkin approach, but the flat and sharp operators do not satisfy the
     exact adjoint relation.
@@ -55,7 +71,9 @@ def local_flat(
             )
 
         case _:
-            raise ValueError()
+            raise ValueError(
+                f"Unknown mode ('{mode}') and mesh dim ({mesh.dim}) combination."
+            )
 
 
 def local_sharp(
@@ -63,19 +81,38 @@ def local_sharp(
     mesh: SimplicialMesh,
     mode: Literal["element", "vertex"],
     location: Literal["barycenter", "circumcenter"] = "barycenter",
-):
+) -> Float[Tensor, "splx coord=3"]:
     """
     Compute the sharp of a 1-cochain using a local, interpolation based method.
 
-    If `mode` is "element", the sharp is computed by interpolating the 1-cochain
-    at either the barycenter (supported for both tri and tet meshes) or the
-    circumcenter (supported for only tri meshes) of the top-level simplices. If
-    `mode` is "vertex", the function first call the element-based method to interpolate
-    the 1-cochain at the barycenters and then compute a per-vertex value by taking
-    an area/volume-weighted average of the interpolated 1-form over all top-level
-    simplices shsaring the vertex as a face. Therefore, the `location` argument
-    is only relevant for tri meshes and element-based sharp.
+    Parameters
+    ----------
+    cochain_1 : [edge,]
+        The input 1-cochain.
+    mesh
+        A simplicial mesh.
+    mode
+        How the sharp is computed. If `mode` is "element", the sharp is computed
+        by interpolating the 1-cochain at either the barycenter or the circumcenter
+        of the top-level simplices. If `mode` is "vertex", the function first call
+        the element-based method to interpolate the 1-cochain at the barycenters
+        and then compute a per-vertex value by taking an area/volume-weighted
+        average of the interpolated 1-form over all top-level simplices sharing
+        the vertex as a face.
+    location
+        Whether to perform interpolation at the barycenter (supported for both
+        tri and tet meshes) or circumcenter (supported for only tri meshes); this
+        is only relevant for tri meshes and element-based sharp.
 
+    Returns
+    -------
+    [splx, coord]
+        The sharp of the input 1-cochain. The first `splx` dimension can refer
+        to either the top-level simplices or the vertices of the mesh, depending
+        on the `mode` argument.
+
+    Notes
+    -----
     Note that this local, interpolation based method is in general faster than
     the Galerkin approach, but the flat and sharp operators do not satisfy the
     exact adjoint relation.
@@ -145,4 +182,6 @@ def local_sharp(
             )
 
         case _:
-            raise ValueError()
+            raise ValueError(
+                f"Unknown mode ('{mode}') and mesh dim ({mesh.dim}) combination."
+            )
