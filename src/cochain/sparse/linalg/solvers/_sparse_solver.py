4e859b913908ec2fe542f2d62e8cbfe3148da7e4
Lu Hong <honglu88@gmail.com>
2026-06-15 22:00:38 -0700

    clarify linear solver adjoint method

--- src/cochain/sparse/linalg/solvers/_sparse_solver.py
+++ src/cochain/sparse/linalg/solvers/_sparse_solver.py
@@ -10,12 +10,28 @@ from ...decoupled_tensor import SparsityPattern
 
 
 class BaseSparseSolver(ABC):
+    """
+    An ABC for sparse linear solver wrapper classes.
+
+    Parameters
+    ----------
+    matrix_type
+        The type of the sparse matrix.
+    dtype
+        The dtype of the sparse matrix.
+    device
+        The device of the sparse matrix.
+    shape
+        The shape of the sparse matrix.
+    """
+
     matrix_type: Literal["general", "symmetric", "spd"]
     dtype: torch.dtype
     device: torch.device
     shape: torch.Size
 
     def size(self, dim: int | None = None) -> int | torch.Size:
+        """Return the shape of the sparse matrix."""
         if dim is None:
             return self.shape
         else:
@@ -30,21 +46,21 @@ class BaseSparseSolver(ABC):
 
 class InvSparseOperator(ABC):
     """
-    An ABC for "stateful" sparse linear solver classes.
+    An ABC for "stateful" differentiable sparse linear solver classes.
 
-    This class provides an abstraction to solving the sparse linear system `A@x=b`
-    for `x`. The tensor `A` is assumed to have shape `(r, c)`, and the tensor `b` is
+    This class provides an abstraction to solving the sparse linear system `a @ x = b`
+    for `x`. The tensor `a` is assumed to have shape `(r, c)`, and the tensor `b` is
     assumed to have shape `(r, *ch)`, and the output `x` tensor is assumed to have
     shape `(c, *ch)`; no explicit leading batch dimensions are allowed.
 
     Parameters
     ----------
     dtype
-        The dtype of the tensor `A`.
+        The dtype of the tensor `a`.
     device
-        The device of the tensor `A`.
+        The device of the tensor `a`.
     shape
-        The shape of the tensor `A`.
+        The shape of the tensor `a`.
     """
 
     dtype: torch.dtype
@@ -53,6 +69,7 @@ class InvSparseOperator(ABC):
     solver: BaseSparseSolver
 
     def size(self, dim: int | None = None) -> int | torch.Size:
+        """Return the shape of the sparse matrix."""
         if dim is None:
             return self.shape
         else:
@@ -69,19 +86,19 @@ class SparseSolverAutogradFunction(torch.autograd.Function):
     def forward(
         a_val: Float[Tensor, " nz"],
         a_pattern: Integer[SparsityPattern, "*b r c"],
-        b: Float[Tensor, " r *ch_flat"] | Float[Tensor, "b r *ch_flat"],
+        b_flat: Float[Tensor, " r *ch_flat"] | Float[Tensor, "b r *ch_flat"],
         solver: BaseSparseSolver,
         trans: Literal["N", "T"],
         persistent: bool,
     ) -> Float[Tensor, " c *ch_flat"] | Float[Tensor, "b c *ch_flat"]:
-        x = solver.solve(b, trans=trans)
-        return x
+        x_flat = solver.solve(b_flat, trans=trans)
+        return x_flat
 
     @staticmethod
     def setup_context(ctx, inputs, output):
         a_val, a_pattern, b, solver, trans, persistent = inputs
 
-        ctx.save_for_backward(output)  # x
+        ctx.save_for_backward(output)  # x_flat
         ctx.a_pattern = a_pattern
         ctx.solver = solver
         ctx.trans = trans
@@ -91,7 +108,7 @@ class SparseSolverAutogradFunction(torch.autograd.Function):
     @once_differentiable
     def backward(
         ctx,
-        dLdx: Float[Tensor, " c *ch_flat"] | Float[Tensor, "b c *ch_flat"],
+        dLdx_flat: Float[Tensor, " c *ch_flat"] | Float[Tensor, "b c *ch_flat"],
     ) -> tuple[
         Float[Tensor, " nz"] | None,
         None,
@@ -101,9 +118,9 @@ class SparseSolverAutogradFunction(torch.autograd.Function):
         None,
     ]:
         needs_grad_a_val = ctx.needs_input_grad[0]
-        needs_grad_b = ctx.needs_input_grad[2]
+        needs_grad_b_flat = ctx.needs_input_grad[2]
 
-        if not (needs_grad_a_val or needs_grad_b):
+        if not (needs_grad_a_val or needs_grad_b_flat):
             return (None,) * 6
 
         if ctx.solver is None:
@@ -111,53 +128,70 @@ class SparseSolverAutogradFunction(torch.autograd.Function):
         else:
             solver: BaseSparseSolver = ctx.solver
 
-        (x,) = ctx.saved_tensors
+        (x_flat,) = ctx.saved_tensors
         a_pattern: SparsityPattern = ctx.a_pattern
 
+        # The backward pass solves the adjoint system.
+        trans = "T" if ctx.trans == "N" else "N"
+
         dLda_val = None
-        dLdb = None
+        dLdb_flat = None
 
-        if needs_grad_b or needs_grad_a_val:
-            # lambda_ has the same shape as b.
-            # For splu(), b has shape [r, *ch]
-            # For nvmath DirectSolver(), b has shape [r, *ch] or [b, r, *ch]
-            lambda_ = solver.solve(dLdx, trans="T" if ctx.trans == "N" else "N")
+        # Currently supported A @ x = b shape combinations:
+        #
+        # For splu():
+        # -------------------------------------------
+        # a.shape    b_flat.shape     x_flat.shape
+        # -------------------------------------------
+        # [r, c]     [r,]             [c,]
+        # [r, c]     [r, ch_flat]     [c, ch_flat]
+        # -------------------------------------------
+        #
+        # For DirectSolver():
+        # -------------------------------------------
+        # a.shape    b_flat.shape     x_flat.shape
+        # -------------------------------------------
+        # [r, c]     [r,]             [c,]
+        # [r, c]     [r, ch_flat]     [c, ch_flat]
+        # [b, r, c]  [b, r, 1]        [b, c, 1]
+        # [b, r, c]  [b, r, ch_flat]  [b, c, ch_flat]
+        # -------------------------------------------
 
-        if needs_grad_b:
-            dLdb = lambda_
+        if needs_grad_b_flat or needs_grad_a_val:
+            # lambda_ has the same shape as b_flat.
+            lambda_ = solver.solve(dLdx_flat, trans=trans)
+
+        if needs_grad_b_flat:
+            dLdb_flat = lambda_
 
         if needs_grad_a_val:
-            # dLdA will have the same sparsity pattern as A.
-            if lambda_.ndim > 1:
-                # Sum over the channel dimension while accounting for zero or more
-                # batch dimensions.
+            # Compute the nonzero values of dLdA using the outer product between
+            # λ and x; dLdA will have the same sparsity pattern as A.
 
-                # Recall that A.pattern.idx_coo has shape (sp, nnz), where sp has size
-                # equal to the number of batch dims plus 2 (i.e., sp = len(*b) + 2).
-                n_batch = a_pattern.n_batch_dim
+            a_has_batch_dim = a_pattern.n_batch_dim > 0
+            b_has_ch_dim = lambda_.ndim > 1
 
-                # Extract the nonzero dLdA element row and col indices, accounting for
-                # batch dimensions, and use them to extract the corresponding elements
-                # from lambda_ and x to construct the nonzero outer product elements.
-                r_idx: Integer[Tensor, "n_batch+1 nnz"] = a_pattern.idx_coo[
-                    list(range(n_batch)) + [n_batch]
-                ]
-                c_idx: Integer[Tensor, "n_batch+1 nnz"] = a_pattern.idx_coo[
-                    list(range(n_batch)) + [n_batch + 1]
-                ]
-                # Note that r_idx.unbind(0) is equivalent to *r_idx for indexing.
-                dLda_val = torch.sum(
-                    -lambda_[r_idx.unbind(0)] * x[c_idx.unbind(0)], dim=-1
-                )
+            a_nz_idx = a_pattern.idx_coo.unbind(dim=0)
 
-            else:
-                # if there are no batch dimensions, then the a_pattern.idx_coo is of
-                # shape (sp=2, nnz).
-                dLda_val = -lambda_[a_pattern.idx_coo[0]] * x[a_pattern.idx_coo[1]]
+            match (b_has_ch_dim, a_has_batch_dim):
+                case (True, True):
+                    b, r, c = a_nz_idx
+                    dLda_val = -torch.sum(lambda_[b, r] * x_flat[b, c], dim=-1)
+
+                case (True, False):
+                    r, c = a_nz_idx
+                    dLda_val = -torch.sum(lambda_[r] * x_flat[c], dim=-1)
+
+                case (False, False):
+                    r, c = a_nz_idx
+                    dLda_val = -lambda_[r] * x_flat[c]
+
+                case (False, True):
+                    raise NotImplementedError()
 
         # Free solver memory if not persistent.
         if not ctx.persistent:
             solver.free()
             ctx.solver = None
 
-        return (dLda_val, None, dLdb, None, None, None)
+        return (dLda_val, None, dLdb_flat, None, None, None)
