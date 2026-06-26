9da69845a1063b78b47dc0af4750b7c6a10f12e2
Lu Hong <honglu88@gmail.com>
2026-06-25 21:28:33 -0700

    docstrings for the synthetic eigenvalue matrix generators

--- tests/sparse/linalg/eigen/conftest.py
+++ tests/sparse/linalg/eigen/conftest.py
@@ -10,6 +10,16 @@ from torch import Tensor
 def givens_rotation_matrix(
     i: int, j: int, theta: float, n: int
 ) -> Float[Tensor, "n n"]:
+    """
+    Compute a Givens rotation matrix.
+
+    For a plane defined by two coordinate axes i and j, a Givens rotation
+    matrix is an orthogonal matrix that represents rotations within the plane
+    by an angle of θ. More specifically, the Givens rotation is an (n, n)
+    identity matrix with the (i, i) and (j, j) elements replaced by cos(θ),
+    the (i, j) element replaced by -sin(θ), and the (j, i) element replaced by
+    sin(θ).
+    """
     cos = math.cos(theta)
     sin = math.sin(theta)
 
@@ -31,6 +41,15 @@ def givens_rotation_matrix(
 
 
 def rand_sp_sym_matrix(lambdas: Float[Tensor, " n"], k: int) -> Float[Tensor, "n n"]:
+    """
+    Generate a random, sparse, symmetric matrix with a predefined set of eigenvalues.
+
+    Let A be a diagonal matrix whose elements are the λ's. This function generates
+    a Givens matrix G for a random coordinate plane and angle, and performs a similarity
+    transformation A <- G @ A @ G.T, which preserves the eigenvalues of the original
+    A, and this process is repeated k times. A larger k results in a more "scrambled",
+    denser output matrix.
+    """
     n = lambdas.size(0)
 
     mat = torch.sparse_coo_tensor(
@@ -52,6 +71,25 @@ def rand_sp_sym_matrix(lambdas: Float[Tensor, " n"], k: int) -> Float[Tensor, "n
 def rand_sym_gep_matrices(
     lambdas: Float[Tensor, " n"], rho: float, k: int
 ) -> tuple[Float[Tensor, "n n"], Float[Tensor, "n n"]]:
+    """
+    Generate two matrices that satisfy a GEP with a predefined set of eigenvalues.
+
+    Let Λ be a diagonal matrix whose diagonal values are the predefined
+    eigenvalues. Define an (n, n) sparse and diagonally dominant "stencil" matrix
+    S with off-diagonal values drawn from the standard normal distribution with
+    fill density ρ. Then, define A = S.T @ Λ @ S and B = S.T @ S (note that B is
+    symmetric positive definite). It follows that A and B satisfy the desired GEP;
+    to see why, note that (S.T @ Λ @ S) y = λ (S.T @ S) y can be simplified to
+    Λ x = λ x if we set x = S y and multiply both sides with inv(S.T) on the left.
+    Lastly, we apply random Givens rotations to A and B simultaneously k times,
+    which preserves the original eigenvalue spectrum but further scrambles the
+    matrices and the eigenvectors. More specifically, the Givens rotations weaken
+    the diagonal dominant structure of B (which is inherited from the construction
+    of S). In addition, since Λ x = λ x, the eigenvector x's are aligned with the
+    coordinate axes, and y = inv(S) x are also closely aligned with the coordinate
+    axes (since inv(S) will also be diagonally dominant); the Givens rotations
+    weaken this alignment.
+    """
     n = lambdas.size(0)
 
     # Construct the diagonal eigenvalue matrix.
